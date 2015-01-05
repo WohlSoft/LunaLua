ac7d71e3742827afa8f09928cc2e05e06abf0371
Wohlstand <admin@wohlnet.ru>
2015-01-06 00:27:59 +0300

    Customizable sounds and musics: global and per episode!

--- LunaDll/SdlMusic/MusicManager.cpp
+++ LunaDll/SdlMusic/MusicManager.cpp
@@ -6,296 +6,8 @@
 #include <math.h>
 #include "../libs/ini-reader/INIReader.h"
 
-std::string MusicManager::chunksAliasesList[91]=
-{
-	"sound1",
-	"sound2",
-	"sound3",
-	"sound4",
-	"sound5",
-	"sound6",
-	"sound7",
-	"sound8",
-	"sound9",
-	"sound10",
-	"sound11",
-	"sound12",
-	"sound13",
-	"sound14",
-	"sound15",
-	"sound16",
-	"sound17",
-	"sound18",
-	"sound19",
-	"sound20",
-	"sound21",
-	"sound22",
-	"sound23",
-	"sound24",
-	"sound25",
-	"sound26",
-	"sound27",
-	"sound28",
-	"sound29",
-	"sound30",
-	"sound31",
-	"sound32",
-	"sound33",
-	"sound34",
-	"sound35",
-	"sound36",
-	"sound37",
-	"sound38",
-	"sound39",
-	"sound40",
-	"sound41",
-	"sound42",
-	"sound43",
-	"sound44",
-	"sound45",
-	"sound46",
-	"sound47",
-	"sound48",
-	"sound49",
-	"sound50",
-	"sound51",
-	"sound52",
-	"sound53",
-	"sound54",
-	"sound55",
-	"sound56",
-	"sound57",
-	"sound58",
-	"sound59",
-	"sound60",
-	"sound61",
-	"sound62",
-	"sound63",
-	"sound64",
-	"sound65",
-	"sound66",
-	"sound67",
-	"sound68",
-	"sound69",
-	"sound70",
-	"sound71",
-	"sound72",
-	"sound73",
-	"sound74",
-	"sound75",
-	"sound76",
-	"sound77",
-	"sound78",
-	"sound79",
-	"sound80",
-	"sound81",
-	"sound82",
-	"sound83",
-	"sound84",
-	"sound85",
-	"sound86",
-	"sound87",
-	"sound88",
-	"sound89",
-	"sound90",
-	"sound91"
-};
-
-std::string MusicManager::defaultChunksList[91]=
-{
-	"sound\\player-jump.ogg", 
-	"sound\\stomped.ogg", 
-	"sound\\block-hit.ogg", 
-	"sound\\block-smash.ogg", 
-	"sound\\player-shrink.ogg", 
-	"sound\\player-grow.ogg", 
-	"sound\\mushroom.ogg", 
-	"sound\\player-died.ogg", 
-	"sound\\shell-hit.ogg", 
-	"sound\\player-slide.ogg", 
-	"sound\\item-dropped.ogg", 
-	"sound\\has-item.ogg", 
-	"sound\\camera-change.ogg", 
-	"sound\\coin.ogg", 
-	"sound\\1up.ogg", 
-	"sound\\lava.ogg", 
-	"sound\\warp.ogg", 
-	"sound\\fireball.ogg", 
-	"sound\\level-win.ogg", 
-	"sound\\boss-beat.ogg", 
-	"sound\\dungeon-win.ogg", 
-	"sound\\bullet-bill.ogg", 
-	"sound\\grab.ogg", 
-	"sound\\spring.ogg", 
-	"sound\\hammer.ogg", 
-	"sound\\slide.ogg", 
-	"sound\\newpath.ogg", 
-	"sound\\level-select.ogg", 
-	"sound\\do.ogg", 
-	"sound\\pause.ogg", 
-	"sound\\key.ogg", 
-	"sound\\pswitch.ogg", 
-	"sound\\tail.ogg", 
-	"sound\\racoon.ogg", 
-	"sound\\boot.ogg", 
-	"sound\\smash.ogg", 
-	"sound\\thwomp.ogg", 
-	"sound\\birdo-spit.ogg", 
-	"sound\\birdo-hit.ogg", 
-	"sound\\smb2-exit.ogg", 
-	"sound\\birdo-beat.ogg", 
-	"sound\\npc-fireball.ogg", 
-	"sound\\fireworks.ogg", 
-	"sound\\bowser-killed.ogg", 
-	"sound\\game-beat.ogg", 
-	"sound\\door.ogg", 
-	"sound\\message.ogg", 
-	"sound\\yoshi.ogg", 
-	"sound\\yoshi-hurt.ogg", 
-	"sound\\yoshi-tongue.ogg", 
-	"sound\\yoshi-egg.ogg", 
-	"sound\\got-star.ogg", 
-	"sound\\zelda-kill.ogg", 
-	"sound\\player-died2.ogg", 
-	"sound\\yoshi-swallow.ogg", 
-	"sound\\ring.ogg", 
-	"sound\\dry-bones.ogg", 
-	"sound\\smw-checkpoint.ogg", 
-	"sound\\dragon-coin.ogg", 
-	"sound\\smw-exit.ogg", 
-	"sound\\smw-blaarg.ogg", 
-	"sound\\wart-bubble.ogg", 
-	"sound\\wart-die.ogg", 
-	"sound\\sm-block-hit.ogg", 
-	"sound\\sm-killed.ogg", 
-	"sound\\sm-hurt.ogg", 
-	"sound\\sm-glass.ogg", 
-	"sound\\sm-boss-hit.ogg", 
-	"sound\\sm-cry.ogg", 
-	"sound\\sm-explosion.ogg", 
-	"sound\\climbing.ogg", 
-	"sound\\swim.ogg", 
-	"sound\\grab2.ogg", 
-	"sound\\smw-saw.ogg", 
-	"sound\\smb2-throw.ogg", 
-	"sound\\smb2-hit.ogg", 
-	"sound\\zelda-stab.ogg", 
-	"sound\\zelda-hurt.ogg", 
-	"sound\\zelda-heart.ogg", 
-	"sound\\zelda-died.ogg", 
-	"sound\\zelda-rupee.ogg", 
-	"sound\\zelda-fire.ogg", 
-	"sound\\zelda-item.ogg", 
-	"sound\\zelda-key.ogg", 
-	"sound\\zelda-shield.ogg", 
-	"sound\\zelda-dash.ogg", 
-	"sound\\zelda-fairy.ogg", 
-	"sound\\zelda-grass.ogg", 
-	"sound\\zelda-hit.ogg", 
-	"sound\\zelda-sword-beam.ogg", 
-	"sound\\bubble.ogg"
-};
-
-int MusicManager::chunksChannelsList[91] =
-{
-	31,//"sound\\player-jump.ogg",
-	-1,//"sound\\stomped.ogg",
-	-1,//"sound\\block-hit.ogg",
-	-1,//"sound\\block-smash.ogg",
-	-1,//"sound\\player-shrink.ogg",
-	-1,//"sound\\player-grow.ogg",
-	-1,//"sound\\mushroom.ogg",
-	-1,//"sound\\player-died.ogg",
-	30,//"sound\\shell-hit.ogg",
-	29,//"sound\\player-slide.ogg",
-	-1,//"sound\\item-dropped.ogg",
-	-1,//"sound\\has-item.ogg",
-	-1,//"sound\\camera-change.ogg",
-	-1,//"sound\\coin.ogg",
-	-1,//"sound\\1up.ogg",
-	-1,//"sound\\lava.ogg",
-	-1,//"sound\\warp.ogg",
-	-1,//"sound\\fireball.ogg",
-	-1,//"sound\\level-win.ogg",
-	-1,//"sound\\boss-beat.ogg",
-	-1,//"sound\\dungeon-win.ogg",
-	-1,//"sound\\bullet-bill.ogg",
-	-1,//"sound\\grab.ogg",
-	-1,//"sound\\spring.ogg",
-	-1,//"sound\\hammer.ogg",
-	-1,//"sound\\slide.ogg",
-	-1,//"sound\\newpath.ogg",
-	-1,//"sound\\level-select.ogg",
-	-1,//"sound\\do.ogg",
-	-1,//"sound\\pause.ogg",
-	-1,//"sound\\key.ogg",
-	28,//"sound\\pswitch.ogg",
-	-1,//"sound\\tail.ogg",
-	-1,//"sound\\racoon.ogg",
-	-1,//"sound\\boot.ogg",
-	27,//"sound\\smash.ogg",
-	-1,//"sound\\thwomp.ogg",
-	-1,//"sound\\birdo-spit.ogg",
-	-1,//"sound\\birdo-hit.ogg",
-	-1,//"sound\\smb2-exit.ogg",
-	-1,//"sound\\birdo-beat.ogg",
-	-1,//"sound\\npc-fireball.ogg",
-	-1,//"sound\\fireworks.ogg",
-	-1,//"sound\\bowser-killed.ogg",
-	-1,//"sound\\game-beat.ogg",
-	-1,//"sound\\door.ogg",
-	-1,//"sound\\message.ogg",
-	-1,//"sound\\yoshi.ogg",
-	-1,//"sound\\yoshi-hurt.ogg",
-	26,//"sound\\yoshi-tongue.ogg",
-	-1,//"sound\\yoshi-egg.ogg",
-	-1,//"sound\\got-star.ogg",
-	-1,//"sound\\zelda-kill.ogg",
-	-1,//"sound\\player-died2.ogg",
-	-1,//"sound\\yoshi-swallow.ogg",
-	-1,//"sound\\ring.ogg",
-	-1,//"sound\\dry-bones.ogg",
-	-1,//"sound\\smw-checkpoint.ogg",
-	-1,//"sound\\dragon-coin.ogg",
-	-1,//"sound\\smw-exit.ogg",
-	-1,//"sound\\smw-blaarg.ogg",
-	-1,//"sound\\wart-bubble.ogg",
-	-1,//"sound\\wart-die.ogg",
-	-1,//"sound\\sm-block-hit.ogg",
-	-1,//"sound\\sm-killed.ogg",
-	-1,//"sound\\sm-hurt.ogg",
-	-1,//"sound\\sm-glass.ogg",
-	-1,//"sound\\sm-boss-hit.ogg",
-	-1,//"sound\\sm-cry.ogg",
-	-1,//"sound\\sm-explosion.ogg",
-	25,//"sound\\climbing.ogg",
-	24,//"sound\\swim.ogg",
-	-1,//"sound\\grab2.ogg",
-	-1,//"sound\\smw-saw.ogg",
-	-1,//"sound\\smb2-throw.ogg",
-	-1,//"sound\\smb2-hit.ogg",
-	-1,//"sound\\zelda-stab.ogg",
-	-1,//"sound\\zelda-hurt.ogg",
-	-1,//"sound\\zelda-heart.ogg",
-	-1,//"sound\\zelda-died.ogg",
-	-1,//"sound\\zelda-rupee.ogg",
-	-1,//"sound\\zelda-fire.ogg",
-	-1,//"sound\\zelda-item.ogg",
-	-1,//"sound\\zelda-key.ogg",
-	-1,//"sound\\zelda-shield.ogg",
-	-1,//"sound\\zelda-dash.ogg",
-	-1,//"sound\\zelda-fairy.ogg",
-	-1,//"sound\\zelda-grass.ogg",
-	-1,//"sound\\zelda-hit.ogg",
-	-1,//"sound\\zelda-sword-beam.ogg",
-	-1,//"sound\\bubble.ogg"
-};
-
-/*
-typedef std::pair<int, std::string> musicFile;
-*/
-
 std::string MusicManager::chunksList[91];
+std::string MusicManager::musList[74];
 
 std::map<std::string, musicFile > MusicManager::registredFiles;
 std::map<std::string, chunkFile > MusicManager::chunksBuffer;
@@ -303,13 +15,15 @@ std::map<std::string, chunkFile > MusicManager::chunksBuffer;
 std::string MusicManager::defaultSndINI="";
 std::string MusicManager::defaultMusINI="";
 
+std::string MusicManager::curRoot="";
+
 void MusicManager::addSound(std::string alias, std::string fileName)
 {
 	bool firstRun = !PGE_SDL_Manager::isInit;
 	PGE_SDL_Manager::initSDL();
 	if(firstRun)
 	{
-		initChunks();
+		initArrays();
 		defaultSndINI=PGE_SDL_Manager::appPath+"sounds.ini";
 		defaultMusINI=PGE_SDL_Manager::appPath+"music.ini";
 		loadSounds(defaultSndINI, PGE_SDL_Manager::appPath);
@@ -325,6 +39,7 @@ void MusicManager::addSound(std::string alias, std::string fileName)
 	std::string s(fileName);
 
 	int chanID=0;
+	int musID=0;
 	//Check is this an SMBX Sound file
 	for(int i=0;i<91;i++)
 	{
@@ -339,14 +54,27 @@ void MusicManager::addSound(std::string alias, std::string fileName)
 	musicFile file;
 	if(isChunk)
 	{
-		file.first=Chunk;
+		file.first = Chunk;
 		//Wroting another path to file
-		file.second=PGE_SDL_Manager::appPath+chunksList[chanID];
+		file.second = chunksList[chanID];
 	}
 	else
 	{
+		bool found=false;
+		for(int i=0; i<74; i++)
+		{
+			if(musAliasesList[i]==alias)
+			{
+				found=true;
+				musID=i;
+				break;
+			}
+		}
 		file.first=Stream;
-		file.second=fileName;
+		if(found)
+			file.second=musList[musID];
+		else
+			file.second=fileName;
 	}
 			
 	registredFiles[alias]=file;
@@ -427,26 +155,25 @@ void MusicManager::pause()
 
 void MusicManager::stop(std::string alias)
 {
-		std::map<std::string, musicFile>::iterator it = registredFiles.find(alias);
-		if(it != registredFiles.end())
+	std::map<std::string, musicFile>::iterator it = registredFiles.find(alias);
+	if(it != registredFiles.end())
+	{
+		musicFile file = registredFiles[alias];
+		if(file.first==Stream)
 		{
-			musicFile file = registredFiles[alias];
-			if(file.first==Stream)
+			PGE_MusPlayer::MUS_stopMusic();
+		}
+		else
+		if(file.first==Chunk)
+		{
+			std::map<std::string, chunkFile >::iterator it = chunksBuffer.find(alias);
+			if(it != chunksBuffer.end())
 			{
-				PGE_MusPlayer::MUS_stopMusic();
-			}
-			else
-			if(file.first==Chunk)
-			{
-				std::map<std::string, chunkFile >::iterator it = chunksBuffer.find(alias);
-				if(it != chunksBuffer.end())
-				{
-					if(chunksBuffer[alias].first>=0)
-						Mix_HaltChannel(chunksBuffer[alias].first);
-				}
+				if(chunksBuffer[alias].first>=0)
+					Mix_HaltChannel(chunksBuffer[alias].first);
 			}
 		}
-
+	}
 }
 
 void MusicManager::setVolume(int _volume)
@@ -479,6 +206,8 @@ void MusicManager::loadSounds(std::string path, std::string root)
 		return;
 	}
 
+	curRoot=root;
+
 	for(int i=0; i<91; i++)
 	{
 		bool valid=false;
@@ -493,40 +222,119 @@ void MusicManager::loadSounds(std::string path, std::string root)
 
 		if( file_existsX(root+fileName) )
 		{
-			chunksList[i] = fileName.c_str();
+			chunksList[i] = root+fileName.c_str();
 		}
 	}
 }
 
 void MusicManager::loadMusics(std::string path, std::string root)
 {
+		if( !file_existsX(path) ) return;
 
+	INIReader MusicIni( path.c_str() );
+	if (MusicIni.ParseError() < 0)
+	{
+		MessageBoxA(0, std::string(path+"\n\nError of read INI file").c_str(), "Error", 0);
+		return;
+	}
+
+	curRoot=root;
+	int i=0;
+
+	//World music
+	for(int j=1; (j<=16) && (i<74); i++, j++)
+	{
+		bool valid=false;
+		std::string head = "world-music-"+i2str(j);
+		std::string fileName;
+
+		fileName = MusicIni.Get(head, "file", "");
+		if(fileName.size()==0) continue;
+		replaceSubStr(fileName, "\"", "");
+		replaceSubStr(fileName, "\\\\",  "\\");
+		replaceSubStr(fileName, "/",  "\\");
+		if( file_existsX(root+fileName) )
+		{
+			musList[i] = root+fileName.c_str();
+		}
+	}
+
+	//Special music
+	for(int j=1; (j<=3) && (i<74); i++, j++)
+	{
+		bool valid=false;
+		std::string head = "special-music-"+i2str(j);
+		std::string fileName;
+
+		fileName = MusicIni.Get(head, "file", "");
+		if(fileName.size()==0) continue;
+		replaceSubStr(fileName, "\"", "");
+		replaceSubStr(fileName, "\\\\",  "\\");
+		replaceSubStr(fileName, "/",  "\\");
+		if( file_existsX(root+fileName) )
+		{
+			musList[i] = root+fileName.c_str();
+		}
+	}
+
+	//Level music
+	for(int j=1; (j<=56) && (i<74); i++, j++)
+	{
+		if(j==24) j++;
+		bool valid=false;
+		std::string head = "level-music-"+i2str(j);
+		std::string fileName;
+
+		fileName = MusicIni.Get(head, "file", "");
+		if(fileName.size()==0) continue;
+		replaceSubStr(fileName, "\"", "");
+		replaceSubStr(fileName, "\\\\",  "\\");
+		replaceSubStr(fileName, "/",  "\\");
+		if( file_existsX(root+fileName) )
+		{
+			musList[i] = root+fileName.c_str();
+		}
+	}
 }
 
 void MusicManager::loadCustomSounds(std::string episodePath)
 {
-	initChunks();
+	initArrays();
 	loadSounds(defaultSndINI, PGE_SDL_Manager::appPath);
 	loadSounds(episodePath+"\\sounds.ini", episodePath);
+	loadMusics(episodePath+"\\music.ini", episodePath);
 
 	for(int i=0; i<91; i++)
 		addSound(chunksAliasesList[i], "dummy");
+	for(int i=0; i<74; i++)
+		addSound(musAliasesList[i], "dummy");
 }
 
 
 void MusicManager::resetSoundsToDefault()
 {
-	initChunks();
+	initArrays();
 	loadSounds(defaultSndINI, PGE_SDL_Manager::appPath);
+	loadMusics(defaultMusINI, PGE_SDL_Manager::appPath);
 	for(int i=0; i<91; i++)
 		addSound(chunksAliasesList[i], "dummy");
+	for(int i=0; i<74; i++)
+		addSound(musAliasesList[i], "dummy");
 }
 
 
-void MusicManager::initChunks()
+void MusicManager::initArrays()
 {
+	curRoot = PGE_SDL_Manager::appPath;
 	for(int i=0; i<91; i++)
-		chunksList[i]=defaultChunksList[i];
+		chunksList[i]=PGE_SDL_Manager::appPath+defaultChunksList[i];
+	for(int i=0; i<74; i++)
+		musList[i] = PGE_SDL_Manager::appPath+defaultMusList[i];
+}
+
+std::string MusicManager::SndRoot()
+{
+	return curRoot;
 }
 
 #endif
--- LunaDll/SdlMusic/MusicManager.h
+++ LunaDll/SdlMusic/MusicManager.h
@@ -32,16 +32,27 @@ class MusicManager
 		static void resetSoundsToDefault();
 		static void loadSounds(std::string path, std::string root);
 		static void loadMusics(std::string path, std::string root);
-	
+		static std::string SndRoot();
 	private:
 		static std::map<std::string, musicFile > registredFiles;
 		static std::map<std::string, chunkFile > chunksBuffer;
 
+		static std::string curRoot;//Current rood directory (episode or application dir)
+
+		//Musics
+		static std::string musList[74];//List of currently loaded files
+		static std::string defaultMusList[74];//List of system default files
+		static std::string musAliasesList[74];//List of reserved aliases for sound effects
+
+		//SFX
 		static std::string chunksList[91];//List of currently loaded files
 		static std::string defaultChunksList[91];//List of system default files
 		static std::string chunksAliasesList[91];//List of reserved aliases for sound effects
-		static void initChunks();//Fill chinks list with system default files
 		static int chunksChannelsList[91];//List of channel reservation by some files (-1 is allowing mixed playback)
+
+		static void initArrays();//Fill chinks and musics list with system default files
+		
+		//INI Paths
 		static std::string defaultSndINI;//Full path to global sounds.ini file
 		static std::string defaultMusINI;//Full path to global music.ini file
 };
