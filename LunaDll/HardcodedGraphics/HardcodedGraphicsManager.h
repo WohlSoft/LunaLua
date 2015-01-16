7a6fa4d310ff095eabfbb83d7d37510edf2b37da
KevinW1998 <kevin.waldock@gmail.com>
2015-01-16 21:13:48 +0100

    Small fix

---
+++ LunaDll/HardcodedGraphics/HardcodedGraphicsManager.h
@@ -0,0 +1,28 @@
+#ifndef HardcodedGraphicsManager_hhh
+#define HardcodedGraphicsManager_hhh
+
+#include <string>
+
+class HardcodedGraphicsManager
+{
+public:
+	HardcodedGraphicsManager(void);
+	~HardcodedGraphicsManager(void);
+
+	
+	//************************************
+	// Method:    patchGraphics
+	// FullName:  HardcodedGraphicsManager::patchGraphics
+	// Access:    public 
+	// Returns:   int -1 if Successful, -2 If file not found or the size of the file if too big to be patched.
+	// Qualifier:
+	// Parameter: void * offset The memory offset from the beginning of the EXE file.
+	// Parameter: char * filepath The image file to patch
+	// Parameter: int sizeOfFile The size of the original image memory segment
+	//************************************
+	int patchGraphics(void* offset, char* filepath, int sizeOfMemory);
+
+    void loadGraphics(std::string imgRootPath);
+};
+
+#endif
