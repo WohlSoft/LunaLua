323288ec362a1a73b6b5a332711f62204bd5bdce
KevinW1998 <kevin.waldock@gmail.com>
2015-04-08 22:52:16 +0200

    Extend LunaLoader to pass also custom paths/names of smbx.
    Also switched from unicode to multi-byte character set.

--- LunaLoader/LunaLoader.cpp
+++ LunaLoader/LunaLoader.cpp
@@ -5,22 +5,86 @@
 #include "tchar.h"
 #include "windows.h"
 #include <string>
+#include <vector>
 
 #define ErrorBox(title, msg) MessageBox(NULL, _T(title), _T(msg), MB_ICONERROR)
+
+std::vector<std::string> splitCmdArgs(std::string str)
+{
+    std::vector<std::string> args;
+    std::string arg;
+    arg.clear();
+    bool quote_opened = false;
+    for (unsigned int i = 0; i < str.size(); i++)
+    {
+        if (quote_opened)
+            goto qstr;
+        if (str[i] == ' ')
+        {
+            if (!arg.empty())
+                args.push_back(arg);
+            arg.clear();
+            continue;
+        }
+        if (str[i] == '\"')
+        {
+            quote_opened = true;
+            continue;
+        }
+        arg.push_back(str[i]);
+        continue;
+
+    qstr:
+        if (str[i] == '\"')
+        {
+            if (!arg.empty())
+                args.push_back(arg);
+            arg.clear();
+            quote_opened = false;
+            continue;
+        }
+        arg.push_back(str[i]);
+    }
+
+    if (!arg.empty())
+        args.push_back(arg);
+
+    return args;
+}
 
 int APIENTRY _tWinMain(HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
     LPTSTR    lpCmdLine,
     int       nCmdShow)
 {
+    std::vector<std::string> cmdArgs = splitCmdArgs(lpCmdLine);
+    std::string newCmdLine = lpCmdLine;
+
+    std::string pathToSMBX = "smbx.exe"; //use either smbx.exe or the first arg
+    if (cmdArgs.size() > 0){ //if more than one arg then possible of a smbx path
+        if (cmdArgs[0].find("--") == std::string::npos){ //if the first arg starting with "--" then no smbx path --> just starting with argument
+            pathToSMBX = cmdArgs[0];
+
+            newCmdLine = ""; //reset the arg path
+            if (cmdArgs.size() > 1){ //strip the smbx-path from the arguments
+                for (unsigned int i = 1; i < cmdArgs.size(); ++i){
+                    newCmdLine += cmdArgs[i] + std::string(" ");
+                }
+            }
+        }
+    }
+
+    char* cmdLineNonConst = new char[newCmdLine.length() + 1];
+    memcpy((void*)cmdLineNonConst, (const void*)newCmdLine.c_str(), newCmdLine.length());
+
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
 
     memset(&si, 0, sizeof(si));
     memset(&pi, 0, sizeof(pi));
 
-    if (!CreateProcess(_T("smbx.exe"), // Launch smbx.exe
-        lpCmdLine,        // Command line
+    if (!CreateProcess(pathToSMBX.c_str(), // Launch smbx.exe
+        cmdLineNonConst,        // Command line
         NULL,             // Process handle not inheritable
         NULL,             // Thread handle not inheritable
         FALSE,            // Set handle inheritance to FALSE
@@ -31,7 +95,7 @@ int APIENTRY _tWinMain(HINSTANCE hInstance,
         &pi)              // Pointer to PROCESS_INFORMATION structure
         )
     {
-        ErrorBox("Error: Couldn't run SMBX", "Couldn't run SMBX");
+        ErrorBox("Error: Couldn't run SMBX\nHowever you can try to drag and drop the SMBX exe file to LunaLoader.exe\nIf this doesn't work then ask on the forums.", "Couldn't run SMBX");
         return 1;
     }
 
