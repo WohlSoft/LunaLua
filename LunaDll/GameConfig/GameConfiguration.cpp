16d01ac11e94be3f40da355d340105451e6a2238
Bluenaxela <Bluenaxela@users.noreply.github.com>
2015-05-20 23:29:29 -0700

    No need for a big complicated TEXTPATCH. VB6StrPtr can make things much simpler.

--- LunaDll/GameConfig/GameConfiguration.cpp
+++ LunaDll/GameConfig/GameConfiguration.cpp
@@ -1,32 +1,30 @@
 #include "GameConfiguration.h"
 #include "../Misc/VB6StrPtr.h"
+#include "../Defines.h"
 
 namespace gameCharacters{
-    VB6StrPtr* marioGame = 0;
-    VB6StrPtr* luigiGame = 0;
-    VB6StrPtr* peachGame = 0;
-    VB6StrPtr* toadGame = 0;
-    VB6StrPtr* linkGame = 0;
+    VB6StrPtr& marioGame = *(VB6StrPtr*)0x9339C2;
+    VB6StrPtr& luigiGame = *(VB6StrPtr*)0x933B00;
+    VB6StrPtr& peachGame = *(VB6StrPtr*)0x933C3D;
+    VB6StrPtr& toadGame = *(VB6StrPtr*)0x933D79;
+    VB6StrPtr& linkGame = *(VB6StrPtr*)0x933EB7;
 }
 
 namespace mainMenu{
-    VB6StrPtr* player1Game = 0;
-    VB6StrPtr* player2Game = 0;
-    VB6StrPtr* battleGame = 0;
-    VB6StrPtr* options = 0;
-    VB6StrPtr* exit = 0;
+    VB6StrPtr& player1Game = *(VB6StrPtr*)0x933417;
+    VB6StrPtr& player2Game = *(VB6StrPtr*)0x933479;
+    VB6StrPtr& battleGame = *(VB6StrPtr*)0x9334DB;
+    VB6StrPtr& options = *(VB6StrPtr*)0x93353D;
+    VB6StrPtr& exit = *(VB6StrPtr*)0x93359F;
 }
 
 namespace optionsMenu{
-    VB6StrPtr* player1Controls = 0;
-    VB6StrPtr* player2Controls = 0;
-    VB6StrPtr* fullscreenMode = 0;
-    VB6StrPtr* windowedMode = 0;
-    VB6StrPtr* viewCredits = 0;
+    VB6StrPtr& player1Controls = *(VB6StrPtr*)0x936DD7;
+    VB6StrPtr& player2Controls = *(VB6StrPtr*)0x936E39;
+    VB6StrPtr& fullscreenMode = *(VB6StrPtr*)0x936F10;
+    VB6StrPtr& windowedMode = *(VB6StrPtr*)0x936EAC;
+    VB6StrPtr& viewCredits = *(VB6StrPtr*)0x936F72;
 }
-
-#define TEXTPATCH(d_variable, d_reader, d_section, d_name, d_defaultVal, d_address) d_variable = new VB6StrPtr(d_reader.Get(d_section, d_name, d_defaultVal)); \
-    memcpy((void*)d_address, d_variable, 4)
 
 void GameConfiguration::runPatchByIni(INIReader& reader)
 {
@@ -34,23 +32,22 @@ void GameConfiguration::runPatchByIni(INIReader& reader)
         return;
 
 
-    TEXTPATCH(gameCharacters::marioGame, reader, "game-characters", "mario-game", "MARIO GAME ", 0x9339C2);
-    TEXTPATCH(gameCharacters::luigiGame, reader, "game-characters", "luigi-game", "LUIGI GAME ", 0x933B00);
-    TEXTPATCH(gameCharacters::peachGame, reader, "game-characters", "peach-game", "PEACH GAME ", 0x933C3D);
-    TEXTPATCH(gameCharacters::toadGame, reader, "game-characters", "toad-game", "TOAD GAME ", 0x933D79);
-    TEXTPATCH(gameCharacters::linkGame, reader, "game-characters", "link-game", "LINK GAME ", 0x933EB7);
+    gameCharacters::marioGame = reader.Get("game-characters", "mario-game", gameCharacters::marioGame);
+    gameCharacters::luigiGame = reader.Get("game-characters", "luigi-game", gameCharacters::luigiGame);
+    gameCharacters::peachGame = reader.Get("game-characters", "peach-game", gameCharacters::peachGame);
+    gameCharacters::toadGame = reader.Get("game-characters", "toad-game", gameCharacters::toadGame);
+    gameCharacters::linkGame = reader.Get("game-characters", "link-game", gameCharacters::linkGame);
 
-    TEXTPATCH(mainMenu::player1Game, reader, "main-menu", "player1-game", "1 PLAYER GAME", 0x933417);
-    TEXTPATCH(mainMenu::player2Game, reader, "main-menu", "player2-game", "2 PLAYER GAME", 0x933479);
-    TEXTPATCH(mainMenu::battleGame, reader, "main-menu", "battle-game", "BATTLE GAME", 0x9334DB);
-    TEXTPATCH(mainMenu::options, reader, "main-menu", "options", "OPTIONS", 0x93353D);
-    TEXTPATCH(mainMenu::exit, reader, "main-menu", "exit", "EXIT", 0x93359F);
+    mainMenu::player1Game = reader.Get("main-menu", "player1-game", mainMenu::player1Game);
+    mainMenu::player2Game = reader.Get("main-menu", "player2-game", mainMenu::player2Game);
+    mainMenu::battleGame = reader.Get("main-menu", "battle-game", mainMenu::battleGame);
+    mainMenu::options = reader.Get("main-menu", "options", mainMenu::options);
+    mainMenu::exit = reader.Get("main-menu", "exit", mainMenu::exit);
 
-    TEXTPATCH(optionsMenu::player1Controls, reader, "option-menu", "player1-controls", "PLAYER 1 CONTROLS", 0x936DD7);
-    TEXTPATCH(optionsMenu::player2Controls, reader, "option-menu", "player2-controls", "PLAYER 2 CONTROLS", 0x936E39);
-    TEXTPATCH(optionsMenu::fullscreenMode, reader, "option-menu", "fullscreen-mode", "FULLSCREEN MODE", 0x936F10);
-    TEXTPATCH(optionsMenu::windowedMode, reader, "option-menu", "windowed-mode", "WINDOWED MODE", 0x936EAC);
-    TEXTPATCH(optionsMenu::viewCredits, reader, "option-menu", "view-credits", "VIEW CREDITS", 0x936F72);
-
+    optionsMenu::player1Controls = reader.Get("option-menu", "player1-controls", optionsMenu::player1Controls);
+    optionsMenu::player2Controls = reader.Get("option-menu", "player2-controls", optionsMenu::player2Controls);
+    optionsMenu::fullscreenMode = reader.Get("option-menu", "fullscreen-mode", optionsMenu::fullscreenMode);
+    optionsMenu::windowedMode = reader.Get("option-menu", "windowed-mode", optionsMenu::windowedMode);
+    optionsMenu::viewCredits = reader.Get("option-menu", "view-credits", optionsMenu::viewCredits);
 
 }
