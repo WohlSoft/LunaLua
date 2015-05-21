409e3a96d5692f072b32f106399bce0a4097464f
Bluenaxela <Bluenaxela@users.noreply.github.com>
2015-05-21 01:03:25 -0700

    Make sure to target Win XP

--- LunaDll/httprequest.h
+++ LunaDll/httprequest.h
@@ -81,9 +81,9 @@ extern "C"{
 //  Copyright (C) Microsoft Corporation. All rights reserved.
 //
 //--------------------------------------------------------------------------
-#include <winapifamily.h>
+//#include <winapifamily.h>
 #pragma region Desktop Family
-#if WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
+#if 1 //WINAPI_FAMILY_PARTITION(WINAPI_PARTITION_DESKTOP)
 #pragma once
 #pragma region Desktop Family
 #pragma endregion
