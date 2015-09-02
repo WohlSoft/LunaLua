c551f5f11a3dc3026ae705b78a694533a0061e42
Bluenaxela <Bluenaxela@users.noreply.github.com>
2015-09-01 23:18:55 -0700

    Remove RenderOverrides.cpp/h as it is no longer used.

--- LunaDll/Rendering/RenderOverrides.h
+++
@@ -1,9 +0,0 @@
-#ifndef RenderOverrides_hhhh
-#define RenderOverrides_hhhh
-
-#include <windows.h>
-
-void loadRenderOverrideGfx();
-bool renderOverrideBitBlt(HDC hdcDest, int nXDest, int nYDest, int nWidth, int nHeight, HDC hdcSrc, int nXSrc, int nYSrc, DWORD dwRop);
-
-#endif
