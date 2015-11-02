f5b2d0ad92073625ae7d2c9fd6fed3eb22215a55
Wohlstand <admin@wohlnet.ru>
2015-11-03 01:53:49 +0300

    Update of SDL2_mixer_ext.lib exports Since SDL2_Mixer_ext's API changed

--- LunaDll/libs/sdl/bin/update_exports.bat
+++ LunaDll/libs/sdl/bin/update_exports.bat
@@ -1,3 +1,3 @@
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\dumpbin.exe" /exports SDL2_mixer_ext.dll > SDL2_mixer_ext_exports.txt
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\dumpbin.exe" /exports SDL2_mixer_ext.dll > SDL2_mixer_ext_exports.txt
 pause
-"C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\lib" /def:SDL2_mixer_ext.def /OUT:SDL2_mixer_ext.lib
+"C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\lib" /def:SDL2_mixer_ext.def /OUT:SDL2_mixer_ext.lib
