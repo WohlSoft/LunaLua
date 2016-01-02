fffd9413fea21611135842a7be10332a8bf13298
KevinW1998 <kevin.waldock@gmail.com>
2016-01-03 00:17:47 +0100

    Console is working again

--- LunaDll/GlobalFuncs.cpp
+++ LunaDll/GlobalFuncs.cpp
@@ -591,51 +591,18 @@ std::wstring getLatestConfigFile(const std::wstring& configname)
 
 void RedirectIOToConsole()
 {
-    int hConHandle;
-    long lStdHandle;
     CONSOLE_SCREEN_BUFFER_INFO coninfo;
-    FILE *fp;
 
     // allocate a console for this app
     AllocConsole();
+    freopen("CONOUT$", "w", stdout);
+    freopen("CONIN$", "r", stdin);
+    freopen("CONERR$", "w", stderr);
 
     // set the screen buffer to be big enough to let us scroll text
     GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &coninfo);
     coninfo.dwSize.Y = 500;
     SetConsoleScreenBufferSize(GetStdHandle(STD_OUTPUT_HANDLE), coninfo.dwSize);
-
-    // redirect unbuffered STDOUT to the console
-    lStdHandle = (long)GetStdHandle(STD_OUTPUT_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-
-    fp = _fdopen(hConHandle, "w");
-
-    *stdout = *fp;
-
-    setvbuf(stdout, NULL, _IONBF, 0);
-
-    // redirect unbuffered STDIN to the console
-
-    lStdHandle = (long)GetStdHandle(STD_INPUT_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-
-    fp = _fdopen(hConHandle, "r");
-    *stdin = *fp;
-    setvbuf(stdin, NULL, _IONBF, 0);
-
-    // redirect unbuffered STDERR to the console
-    lStdHandle = (long)GetStdHandle(STD_ERROR_HANDLE);
-    hConHandle = _open_osfhandle(lStdHandle, _O_TEXT);
-
-    fp = _fdopen(hConHandle, "w");
-
-    *stderr = *fp;
-
-    setvbuf(stderr, NULL, _IONBF, 0);
-
-    // make cout, wcout, cin, wcin, wcerr, cerr, wclog and clog
-    // point to console as well
-    std::ios::sync_with_stdio();
 }
 
 #ifdef BUILD_WITH_ATL_STUFF
