9b9c4494a02841985d1ac8744302224caa654c6c
Kevin Waldock <kevin.waldock@gmail.com>
2016-01-13 12:30:55 +0100

    Add FFI_EXPORT for direct FFI signature and new Alloc function.

--- LunaDll/LuaMain/LuaProxyFFI.cpp
+++ LunaDll/LuaMain/LuaProxyFFI.cpp
@@ -4,13 +4,18 @@
 
 #include "../Globals.h"
 
+#define FFI_EXPORT(sig) __declspec(dllexport) sig __cdecl
 
 extern "C" {
-    __declspec(dllexport) float* __cdecl LunaLuaGlAllocCoords(size_t size) {
+    FFI_EXPORT(float*) LunaLuaGlAllocCoords(size_t size) {
         return (float*)malloc(size  * sizeof(float));
     }
 
-    __declspec(dllexport) void __cdecl LunaLuaGlDrawTriangles(const float* vert, const float* tex, unsigned int count) {
+    FFI_EXPORT(void*) LunaLuaAlloc(size_t size) {
+        return malloc(size);
+    }
+
+    FFI_EXPORT(void) LunaLuaGlDrawTriangles(const float* vert, const float* tex, unsigned int count) {
         auto obj = std::make_shared<GLEngineCmd_Draw2DArray>();
         obj->mType = GL_TRIANGLES;
         obj->mVert = vert;
