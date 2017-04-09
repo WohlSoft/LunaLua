25d5eba813d20186b87667a1d85c6ae54aa61bd9
Bluenaxela <Bluenaxela@users.noreply.github.com>
2017-04-09 00:31:39 -0700

    [Cleanup] Switch to a standard cmake-buildable glew-1.13.0

--- CMake/DownloadAndConfigureDepends/glew/glew.cmake
+++ CMake/DownloadAndConfigureDepends/glew/glew.cmake
@@ -17,10 +17,10 @@ find_package(Git REQUIRED)
 set(GLEW_INSTALL_DIR ${CMAKE_BINARY_DIR}/external/glew-install)
 set(GLEW_INCLUDE_DIR ${GLEW_INSTALL_DIR}/include)
 set(GLEW_LIBRARY_DIR ${GLEW_INSTALL_DIR}/lib)
-set(GLEW_LIBRARY_STATIC_FILES ${GLEW_LIBRARY_DIR}/glew$<$<CONFIG:Debug>:d>.lib)
-set(GLEW_LIBRARY_SHARED_FILES ${GLEW_LIBRARY_DIR}/libglew_shared$<$<CONFIG:Debug>:d>.lib)
+set(GLEW_LIBRARY_STATIC_FILES ${GLEW_LIBRARY_DIR}/libglew32$<$<CONFIG:Debug>:d>.lib)
+set(GLEW_LIBRARY_SHARED_FILES ${GLEW_LIBRARY_DIR}/glew32$<$<CONFIG:Debug>:d>.lib)
 set(GLEW_BINARY_DIR ${GLEW_INSTALL_DIR}/lib)
-set(GLEW_BINARY_FILES ${GLEW_BINARY_DIR}/glew$<$<CONFIG:Debug>:d>.dll)
+set(GLEW_BINARY_FILES ${GLEW_BINARY_DIR}/glew32$<$<CONFIG:Debug>:d>.dll)
 
 if(NOT GLEW_BUILD_SHARED)
     add_definitions(-DGLEW_STATIC) 
@@ -28,13 +28,21 @@ endif()
 
 
 ExternalProject_Add(
-    glew
-    PREFIX ${CMAKE_BINARY_DIR}/external/glew
-    # GIT_REPOSITORY https://github.com/Perlmint/glew-cmake.git
-    GIT_REPOSITORY https://github.com/Bluenaxela/glew-cmake.git
-    # GIT_TAG 7be092340b60838bc860bf8bbde01f42b8d51125
-    GIT_TAG bce1c401f7f869be4268f6af9f1bb804f409eab7
-    CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${GLEW_INSTALL_DIR}"
+    glew-download
+    PREFIX ${CMAKE_BINARY_DIR}/external/glew-download
+    URL https://downloads.sourceforge.net/project/glew/glew/1.13.0/glew-1.13.0.zip
+	URL_HASH SHA256=bcbb058a14ef565bd3466864232206973fa801387e693177f42f660599aa6e99
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ""
 )
 
-
+ExternalProject_Add(
+    glew
+	DEPENDS glew-download
+    PREFIX ${CMAKE_BINARY_DIR}/external/glew
+    DOWNLOAD_COMMAND ""
+	SOURCE_DIR ${CMAKE_BINARY_DIR}/external/glew-download/src/glew-download/build/cmake
+    CMAKE_ARGS "-DCMAKE_INSTALL_PREFIX=${GLEW_INSTALL_DIR}"
+)
