b01eb0adb994a850290faddea29deba0b7f8e1ba
Bluenaxela <Bluenaxela@users.noreply.github.com>
2018-04-04 00:41:38 -0700

    Initial support for FFI blocks

--- LunaDll/LuaMain/LuaProxyFFI.cpp
+++ LunaDll/LuaMain/LuaProxyFFI.cpp
@@ -5,6 +5,7 @@
 #include "../Globals.h"
 #include "../Rendering/LunaImage.h"
 #include "../Rendering/FrameCapture.h"
+#include "../SMBXInternal/Blocks.h"
 
 #define FFI_EXPORT(sig) __declspec(dllexport) sig __cdecl
 
@@ -44,5 +45,25 @@ extern "C" {
 	FFI_EXPORT(unsigned int) LunaLuaGetTemplateAddressForCharacter(int id)
 	{
 		return (unsigned int)getTemplateForCharacter(id);
+	}
+
+	FFI_EXPORT(int) LunaLuaBlocksTestCollision(unsigned int plAddr, unsigned int blAddr)
+	{
+		return Blocks::TestCollision((PlayerMOB*)plAddr, (Block*)blAddr);
+	}
+
+	FFI_EXPORT(void) LunaLuaBlockRemove(unsigned int idx, short playSoundEffect)
+	{
+		short doPlaySoundAndEffects = COMBOOL(playSoundEffect);
+		native_removeBlock(&idx, &doPlaySoundAndEffects);
+	}
+
+	FFI_EXPORT(void) LunaLuaBlockHit(unsigned int blockIdx, short fromUpSide, unsigned short playerIdx, int hittingCount)
+	{
+		short unkFlag1VB = COMBOOL(fromUpSide);
+		native_hitBlock(&blockIdx, &unkFlag1VB, &playerIdx);
+		if (hittingCount != -1) {
+			Blocks::Get(blockIdx)->RepeatingHits = hittingCount;
+		}
 	}
 }
