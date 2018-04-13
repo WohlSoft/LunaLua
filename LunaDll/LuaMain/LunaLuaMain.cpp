067e28268253fcfa3b0c23cde7250ee8f27c60df
Bluenaxela <Bluenaxela@users.noreply.github.com>
2018-04-12 22:57:45 -0700

    It wasn't safe to keep the load screen running during a next area's initial Lua

--- LunaDll/LuaMain/LunaLuaMain.cpp
+++ LunaDll/LuaMain/LunaLuaMain.cpp
@@ -120,6 +120,8 @@ void CLunaLua::init(LuaLunaType type, std::wstring codePath, std::wstring levelP
 {
     SafeFPUControl noFPUExecptions;
 
+	LunaLoadScreenKill();
+
     //Just to be safe
     shutdown();
     //Open up a new Lua State
