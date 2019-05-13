cc07e57fb1840005a690a4fe3dc82b18b9109f0d
Vitaly Novichkov <Wohlstand@users.noreply.github.com>
2019-05-13 20:56:40 +0300

    Added a simple script to build LunaLoader-exec

---
+++ build-lunaexec.bat
@@ -0,0 +1,16 @@
+@echo off
+
+set CurDir=%CD%
+set QT_DIR=C:\Qt\5.12.2\mingw73_32
+set MINGW_DIR=C:\Qt\Tools\mingw730_32\
+
+set BUILD_DIR=%CurDir%\build-LunaExec
+set SOURCE_DIR=%CurDir%\LunaLoader
+
+if not exist "%BUILD_DIR%\*" md "%BUILD_DIR%"
+
+cd "%BUILD_DIR%"
+"%QT_DIR%\bin\qmake.exe" CONFIG-=debug CONFIG+=release "%SOURCE_DIR%\LunaLoader.pro"
+"%MINGW_DIR%\bin\mingw32-make.exe" -j 2
+
+echo === Built! ===
