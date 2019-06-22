669b266d9fac84c078ba3d1be8621820890949f5
Bluenaxela <Bluenaxela@users.noreply.github.com>
2019-06-22 13:49:40 -0700

    Avoid reading inputs when window is out of focus, regard to test mode pause menu or Misc.pause state

--- LunaDll/Misc/TestMode.h
+++ LunaDll/Misc/TestMode.h
@@ -63,6 +63,8 @@ LevelData &getCurrentLevelData();
 
 bool TestModeCheckHideWindow(void);
 
+extern HWND GetMainSMBXWindow(void);
+
 // The following function decleration is not included in this header, to
 // avoid applying a json.hpp dependency to this header.
 //   json IPCTestLevel(const json& params)
