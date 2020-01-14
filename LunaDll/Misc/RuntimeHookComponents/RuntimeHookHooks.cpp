WohlSoft/LunaLua

commit ffb80a7b02a25193fb9cd4ca67a7e4db2df94e3e

Allow flying types to be set to collide with semisolids.

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -2323,3 +2323,24 @@ void __stdcall runtimeHookRunAnimInternal(short* effectID, Momentum* coor, float
         runtimeHookRunAnim_OrigFunc(effectID, coor, effectFrame, npcID, showOnlyMask);
     }
 }
+
+static unsigned int __stdcall runtimeHookSemisolidInteractionHook(NPCMOB* npc)
+{
+    unsigned short npcID = npc->id;
+    if (npc_isflying[npcID] != 0) {
+        // Flying NPCs generally don't collide with semisolids... unless it's just a hop?
+        return NPC::CheckSemisolidCollidingFlyType(npc->ai1) ? -1 : 0;
+    }
+    return -1; // Collides
+}
+
+_declspec(naked) void __stdcall runtimeHookSemisolidInteractionHook_Raw()
+{
+    __asm {
+        PUSH ESI
+        CALL runtimeHookSemisolidInteractionHook
+        TEST eax,eax
+        PUSH 0xA12095
+        RET
+    }
+}

LunaDll/LuaMain/LunaLuaMain.cpp
@@ -86,9 +86,6 @@ void CLunaLua::exitContext()
 
         g_PerfTracker.disable();
 
-        gRenderBGOFlag = true;
-        gRenderSizableFlag = true;
-
         // Clean & stop all user started sounds and musics
         PGE_MusPlayer::MUS_stopMusic();
         PGE_Sounds::clearSoundBuffer();
@@ -102,6 +99,10 @@ bool CLunaLua::shutdown()
     //Shutdown the lua module if possible
     if(!isValid())
         return false;
+
+    // Unset flags of things Lua code was processing
+    gRenderBGOFlag = true;
+    gRenderSizableFlag = true;
 
     // Request cached images be held onto for now
     LunaImage::holdCachedImages();
@@ -1129,7 +1130,8 @@ void CLunaLua::bindAll()
                     def("doPSwitchRaw", &LuaProxy::Misc::doPSwitchRaw),
                     def("doPSwitch", (void(*)())&LuaProxy::Misc::doPSwitch),
                     def("doPSwitch", (void(*)(bool))&LuaProxy::Misc::doPSwitch),
-                    def("doBombExplosion", (void(*)(double, double, short))&LuaProxy::Misc::doBombExplosion)
+                    def("doBombExplosion", (void(*)(double, double, short))&LuaProxy::Misc::doBombExplosion),
+                    def("_setSemisolidCollidingFlyType", &NPC::SetSemisolidCollidingFlyType)
                     //def("doBombExplosion", (void(*)(double, double, short, const LuaProxy::Player&))&LuaProxy::Misc::doBombExplosion)
                 ],
 

LunaDll/Misc/RuntimeHook.h
@@ -414,5 +414,7 @@ void __stdcall runtimeHookLoadDefaultControls(void);
 
 void __stdcall runtimeHookRunAnimInternal(short* effectID, Momentum* coor, float* effectFrame, short* npcID, short* showOnlyMask);
 
+void __stdcall runtimeHookSemisolidInteractionHook_Raw();
+
 #endif
 

LunaDll/Misc/RuntimeHookComponents/RuntimeHookGeneral.cpp
@@ -824,6 +824,8 @@ void TrySkipPatch()
     //                     of code that assumes success of GM_RUN_ANIM.
     // PATCH(0x9E7380).JMP(runtimeHookRunAnimInternal).NOP_PAD_TO_SIZE<6>().Apply();
 
+    PATCH(0xA12050).JMP(runtimeHookSemisolidInteractionHook_Raw).NOP_PAD_TO_SIZE<69>().Apply();
+
     /************************************************************************/
     /* Import Table Patch                                                   */
     /************************************************************************/

LunaDll/SMBXInternal/NPCs.cpp
@@ -1,6 +1,7 @@
 #include "NPCs.h"
 #include "../Misc/MiscFuncs.h"
 #include <list>
+#include <unordered_map>
 
 // GET
 NPCMOB* NPC::Get(int index) {
@@ -134,6 +135,9 @@ static int16_t npcprop_ignorethrownnpcs[NPC::MAX_ID + 1] = { 0 };
 static int16_t npcprop_linkshieldable[NPC::MAX_ID + 1] = { 0 };
 static int16_t npcprop_noshieldfireeffect[NPC::MAX_ID + 1] = { 0 };
 static int16_t npcprop_notcointransformable[NPC::MAX_ID + 1] = { 0 };
+
+// Other NPC-related config data, not by ID
+static std::unordered_map<unsigned int, bool> npc_semisolidCollidingFlyTypeMap = { { 1, true } };
 
 // Initialization of inbuilt NPC property arrays
 void NPC::InitProperties() {
@@ -333,6 +337,9 @@ void NPC::InitProperties() {
     npcprop_notcointransformable[222] = -1;
     npcprop_notcointransformable[223] = -1;
     npcprop_notcointransformable[224] = -1;
+
+    npc_semisolidCollidingFlyTypeMap.clear();
+    npc_semisolidCollidingFlyTypeMap[1] = true;
 }
 
 // Internal C++ getters for inbuilt NPC property arrays
@@ -424,4 +431,19 @@ uintptr_t NPC::GetPropertyTableAddress(const std::string& s)
     {
         return 0;
     }
+}
+
+bool NPC::CheckSemisolidCollidingFlyType(unsigned int flyType)
+{
+    auto it = npc_semisolidCollidingFlyTypeMap.find(flyType);
+    if (it != npc_semisolidCollidingFlyTypeMap.end())
+    {
+        return it->second;
+    }
+    return false;
+}
+
+void NPC::SetSemisolidCollidingFlyType(unsigned int flyType, bool shouldCollide)
+{
+    npc_semisolidCollidingFlyTypeMap[flyType] = shouldCollide;
 }

LunaDll/SMBXInternal/NPCs.h
@@ -511,6 +511,9 @@ namespace NPC {
 
     uintptr_t GetPropertyTableAddress(const std::string& s);
 
+    bool CheckSemisolidCollidingFlyType(unsigned int flyType);
+    void SetSemisolidCollidingFlyType(unsigned int flyType, bool shouldCollide);
+
     // NOTE: This is the value we are patching it to. Originally 300
     static const short MAX_ID = 1000;
 }

commit b3107d2fa6ea9c85cbaa5ddc9fd6fe1c3bbb3cc9

Avoid warning caused by ffb80a7b02a25193fb9cd4ca67a7e4db2df94e3e

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -2406,7 +2406,7 @@ static unsigned int __stdcall runtimeHookSemisolidInteractionHook(NPCMOB* npc)
     unsigned short npcID = npc->id;
     if (npc_isflying[npcID] != 0) {
         // Flying NPCs generally don't collide with semisolids... unless it's just a hop?
-        return NPC::CheckSemisolidCollidingFlyType(npc->ai1) ? -1 : 0;
+        return NPC::CheckSemisolidCollidingFlyType((unsigned int)npc->ai1) ? -1 : 0;
     }
     return -1; // Collides
 }