WohlSoft/LunaLua

commit 74252c2bd5e2fc344bc62e81722f0d7ec43b6f9d

Don't load overworld Lua for cross-level warping or test mode restarts

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -156,11 +156,16 @@ extern int __stdcall LoadWorld()
     gSavedVarBank.CheckSaveDeletion();
     gSavedVarBank.CopyBank(&gAutoMan.m_UserVars);
 
-    gLunaLua.init(CLunaLua::LUNALUA_WORLD, (std::wstring)GM_FULLDIR);
-    gLunaLua.setReady(true); // We assume that the SMBX engine is already ready when loading the world
+    // Check that we're not just transiently going though having the world loaded...
 
-    // Overworld is guaranteed to be loaded by this point, so trigger onStart
-    gLunaLua.triggerOnStart();
+    if (GM_NEXT_LEVEL_FILENAME.length() == 0)
+    {
+        gLunaLua.init(CLunaLua::LUNALUA_WORLD, (std::wstring)GM_FULLDIR);
+        gLunaLua.setReady(true); // We assume that the SMBX engine is already ready when loading the world
+
+        // Overworld is guaranteed to be loaded by this point, so trigger onStart
+        gLunaLua.triggerOnStart();
+    }
 
     short plValue = GM_PLAYERS_COUNT;
 #ifndef __MINGW32__

commit cd638892e01b5ecbd449198b3169a5745b29f446

Fix problem caused by 74252c2bd5e2fc344bc62e81722f0d7ec43b6f9d which meant extended characters weren't loaded during a particular excess reset of the player strate, causing a crash.

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -962,7 +962,15 @@ extern void __stdcall FrameTimingMaxFPSHook()
 // Also know as "Player init" hook. This method is called when the player resets.
 extern void __stdcall InitLevelEnvironmentHook()
 {
-    native_initLevelEnv();
+    // This function will fail if the player is a custom character but Lua is not yet loaded
+    // This failure condition would occur for cross level warp and test mode restart, because
+    // that briefly transitions through overworld load state without actually loading Lua.
+    // To work around this, just don't perform an excess unnecessary reset of the player during
+    // this transition.
+    if (GM_NEXT_LEVEL_FILENAME.length() == 0)
+    {
+        native_initLevelEnv();
+    }
 }
 
 static _declspec(naked) void __stdcall msgbox_OrigFunc(unsigned int* pPlayerIdx)