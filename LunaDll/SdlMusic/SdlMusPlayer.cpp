WohlSoft/LunaLua

commit 3d31de4418aa7fb21e61d3925e5b142da893bdd5

Defer music playback until the moment the first frame finishes rendering.

LunaDll/SdlMusic/SdlMusPlayer.cpp
@@ -28,6 +28,9 @@ void PGE_SDL_Manager::initSDL()
 
 /***********************************PGE_MusPlayer********************************************/
 Mix_Music *PGE_MusPlayer::play_mus = NULL;
+bool PGE_MusPlayer::deferringMusic = false;
+bool PGE_MusPlayer::musicGotDeferred = false;
+int PGE_MusPlayer::musicDeferredFadeIn = -1;
 std::string PGE_MusPlayer::currentTrack="";
 int PGE_MusPlayer::sRate=44100;
 bool PGE_MusPlayer::showMsg=true;
@@ -45,15 +48,19 @@ void PGE_MusPlayer::MUS_playMusic()
     if(!PGE_SDL_Manager::isInit) return;
     if(play_mus)
     {
-        if (Mix_PlayingMusic() == 0)
+        if (deferringMusic)
+        {
+            musicGotDeferred = true;
+            musicDeferredFadeIn = -1;
+        }
+        else if (Mix_PlayingMusic() == 0)
         {
             // Reset music sample count
             musSCount.store(0);
 
             Mix_PlayMusic(play_mus, -1);
         }
-        else
-        if(Mix_PausedMusic()==1)
+        else if (Mix_PausedMusic()==1)
         {
             Mix_ResumeMusic();
         }
@@ -70,7 +77,12 @@ void  PGE_MusPlayer::MUS_playMusicFadeIn(int ms)
 
     if(play_mus)
     {
-        if(Mix_PausedMusic()==0)
+        if (deferringMusic)
+        {
+            musicGotDeferred = true;
+            musicDeferredFadeIn = ms;
+        }
+        else if(Mix_PausedMusic()==0)
         {
             // Reset music sample count
             musSCount.store(0);
@@ -82,7 +94,9 @@ void  PGE_MusPlayer::MUS_playMusicFadeIn(int ms)
                 }
         }
         else
+        {
             Mix_ResumeMusic();
+        }
 
     }
     else
@@ -95,19 +109,24 @@ void PGE_MusPlayer::MUS_pauseMusic()
 {
     if(!PGE_SDL_Manager::isInit) return;
     Mix_PauseMusic();
+    musicGotDeferred = false;
 }
 
 void PGE_MusPlayer::MUS_stopMusic()
 {
     if(!PGE_SDL_Manager::isInit) return;
     Mix_HaltMusic();
+    musicGotDeferred = false;
 }
 
 void PGE_MusPlayer::MUS_stopMusicFadeOut(int ms)
 {
     if(!PGE_SDL_Manager::isInit) return;
-    if(Mix_FadingMusic()!=MIX_FADING_OUT)
+    if(Mix_FadingMusic() != MIX_FADING_OUT)
+    {
         Mix_FadeOutMusic(ms);
+        musicGotDeferred = false;
+    }
 }
 
 std::string PGE_MusPlayer::MUS_MusicTitle()
@@ -212,6 +231,7 @@ void PGE_MusPlayer::MUS_openFile(const char *musFile)
         Mix_HaltMusic();
         Mix_FreeMusic(play_mus);
         play_mus=NULL;
+        musicGotDeferred = false;
     }
 
     play_mus = Mix_LoadMUS( musFile );
@@ -256,6 +276,38 @@ unsigned __int64 PGE_MusPlayer::sampleCount()
 unsigned __int64 PGE_MusPlayer::MUS_sampleCount()
 {
     return musSCount;
+}
+
+void PGE_MusPlayer::MUS_StartDeferring()
+{
+    if (deferringMusic) return;
+
+    musicGotDeferred = (Mix_PlayingMusic() == 1) && (Mix_PausedMusic() == 0);
+    if (musicGotDeferred)
+    {
+        musicDeferredFadeIn = -1;
+        MUS_pauseMusic();
+    }
+    deferringMusic = true;
+}
+
+void PGE_MusPlayer::MUS_StopDeferring()
+{
+    if (!deferringMusic) return;
+
+    if (musicGotDeferred)
+    {
+        if (musicDeferredFadeIn >= 0)
+        {
+            MUS_playMusicFadeIn(musicDeferredFadeIn);
+        }
+        else
+        {
+            MUS_playMusic();
+        }
+    }
+    musicGotDeferred = false;
+    deferringMusic = false;
 }
 
 /***********************************PGE_Sounds********************************************/

LunaDll/EventStateMachine.cpp
@@ -123,6 +123,11 @@ void EventStateMachine::hookLevelRenderFirstCameraStart(void) {
 
 void EventStateMachine::hookLevelRenderEnd(void) {
     if (m_onDrawEndReady) {
+        if (m_MusicDeferred)
+        {
+            m_MusicDeferred = false;
+            PGE_MusPlayer::MUS_StopDeferring();
+        }
         sendOnDrawEnd();
     }
 }
@@ -136,10 +141,20 @@ void EventStateMachine::hookWorldRenderStart(void) {
 }
 void EventStateMachine::hookWorldRenderEnd(void) {
     if (m_onDrawEndReady) {
+        if (m_MusicDeferred)
+        {
+            m_MusicDeferred = false;
+            PGE_MusPlayer::MUS_StopDeferring();
+        }
         sendOnDrawEnd();
     }
 }
 
+void EventStateMachine::loadTimeDeferMusic(void)
+{
+    PGE_MusPlayer::MUS_StartDeferring();
+    m_MusicDeferred = true;
+}
 
 // Private methods (Outgoing events)
 void EventStateMachine::sendOnLoop(void) {
@@ -216,11 +231,7 @@ void EventStateMachine::runPause(void) {
         if (!gMainWindowFocused)
         {
             // Pause music if it was playing
-            bool musicWasPlaying = PGE_MusPlayer::MUS_IsPlaying();
-            if (musicWasPlaying)
-            {
-                PGE_MusPlayer::MUS_pauseMusic();
-            }
+            PGE_MusPlayer::MUS_StartDeferring();
 
             // Wait for focus
             while (!gMainWindowFocused && !m_RequestUnpause)
@@ -231,10 +242,7 @@ void EventStateMachine::runPause(void) {
             if (m_RequestUnpause) break;
 
             // Start music again
-            if (musicWasPlaying)
-            {
-                PGE_MusPlayer::MUS_playMusic();
-            }
+            PGE_MusPlayer::MUS_StopDeferring();
         }
 
         // Read input

LunaDll/EventStateMachine.h
@@ -12,9 +12,11 @@ private:
     bool m_RequestUnpause;
     bool m_IsPaused;
 
+    bool m_MusicDeferred;
+
 // Constructor and Destructor
 public:
-    EventStateMachine() { reset(); }
+    EventStateMachine() { m_MusicDeferred = false; reset(); }
     ~EventStateMachine() { }
 
 // Public methods (Notifications of State)
@@ -28,6 +30,7 @@ public:
     void hookLevelRenderEnd(void);
     void hookWorldRenderStart(void);
     void hookWorldRenderEnd(void);
+    void loadTimeDeferMusic(void);
 
 // Private methods (Outgoing events)
 private:

LunaDll/Main.cpp
@@ -218,6 +218,9 @@ int OnLvlLoad() {
 
     if (doSoundLoading) MusicManager::loadCustomSounds(wldPath + "\\", custPath);
 
+    // Start deferring music
+    g_EventHandler.loadTimeDeferMusic();
+
     if(gLunaEnabled) {
         // Load autocode
         gAutoMan.Clear(false);

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -160,6 +160,9 @@ extern int __stdcall LoadWorld()
 
     if (GM_NEXT_LEVEL_FILENAME.length() == 0)
     {
+        // Start deferring music
+        g_EventHandler.loadTimeDeferMusic();
+
         gLunaLua.init(CLunaLua::LUNALUA_WORLD, (std::wstring)GM_FULLDIR);
         gLunaLua.setReady(true); // We assume that the SMBX engine is already ready when loading the world
 

LunaDll/Misc/TestModeMenu.cpp
@@ -77,11 +77,7 @@ void testModePauseMenu(bool allowContinue)
         if (!gMainWindowFocused)
         {
             // Pause music if it was playing
-            bool musicWasPlaying = PGE_MusPlayer::MUS_IsPlaying();
-            if (musicWasPlaying)
-            {
-                PGE_MusPlayer::MUS_pauseMusic();
-            }
+            PGE_MusPlayer::MUS_StartDeferring();
 
             // Wait for focus
             while (!gMainWindowFocused && keepRunningPauseMenu && !GM_EPISODE_MODE)
@@ -92,10 +88,7 @@ void testModePauseMenu(bool allowContinue)
             if (!keepRunningPauseMenu || GM_EPISODE_MODE) break;
 
             // Start music again
-            if (musicWasPlaying)
-            {
-                PGE_MusPlayer::MUS_playMusic();
-            }
+            PGE_MusPlayer::MUS_StopDeferring();
         }
 
         // Read input...

LunaDll/SdlMusic/SdlMusPlayer.h
@@ -48,8 +48,15 @@ public:
 
     static unsigned __int64 sampleCount();
     static unsigned __int64 MUS_sampleCount();
+
+    static void MUS_StartDeferring();
+    static void MUS_StopDeferring();
 private:
     static Mix_Music *play_mus;
+    static bool deferringMusic;
+    static bool musicGotDeferred;
+    static int musicDeferredFadeIn;
+
     static int sRate;
     static bool showMsg;
     static std::string showMsg_for;

commit 563ad7b54e6d53bd3251080fea853ef8c4119831

Fix bug in deferred music playback, introduced by 3d31de4418aa7fb21e61d3925e5b142da893bdd5

LunaDll/SdlMusic/SdlMusPlayer.cpp
@@ -310,8 +310,10 @@ void PGE_MusPlayer::MUS_StopDeferring()
 {
     if (!deferringMusic) return;
 
+    deferringMusic = false;
     if (musicGotDeferred)
     {
+        musicGotDeferred = false;
         if (musicDeferredFadeIn >= 0)
         {
             MUS_playMusicFadeIn(musicDeferredFadeIn);
@@ -321,8 +323,6 @@ void PGE_MusPlayer::MUS_StopDeferring()
             MUS_playMusic();
         }
     }
-    musicGotDeferred = false;
-    deferringMusic = false;
 }
 
 /***********************************PGE_Sounds********************************************/