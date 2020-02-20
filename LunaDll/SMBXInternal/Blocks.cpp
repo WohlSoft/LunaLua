WohlSoft/LunaLua

commit 8a2aa1fd7edcfc98565f8b1550a9016265820ecf

Fix player filter block bouncing glitch. Also, allow playerfilter to be configured, and allow -1 to allow all players

LunaDll/SMBXInternal/Blocks.cpp
@@ -111,11 +111,14 @@ void Blocks::HideAll(int type) {
     }
 }
 
-static bool isBlockBumpable[Block::MAX_ID] = {false};
+static int16_t blockprop_bumpable[Block::MAX_ID + 1] = { 0 };
+static int16_t blockprop_playerfilter[Block::MAX_ID + 1] = { 0 };
+
 void Blocks::InitProperties() {
     for (int id = 1; id <= Block::MAX_ID; id++)
     {
         SetBlockBumpable(id, false);
+        SetBlockPlayerFilter(id, 0);
     }
 
     // Default game config
@@ -139,14 +142,49 @@ void Blocks::InitProperties() {
     {
         SetBlockBumpable(id, true);
     }
+
+    SetBlockPlayerFilter(626, 1);
+    SetBlockPlayerFilter(627, 2);
+    SetBlockPlayerFilter(628, 3);
+    SetBlockPlayerFilter(629, 4);
+    SetBlockPlayerFilter(632, 5);
 }
 
 bool Blocks::GetBlockBumpable(int id) {
     if ((id < 1) || (id > Block::MAX_ID)) return false;
-    return isBlockBumpable[id - 1];
+    return (blockprop_bumpable[id - 1] != 0);
 }
 
 void Blocks::SetBlockBumpable(int id, bool bumpable) {
     if ((id < 1) || (id > Block::MAX_ID)) return;
-    isBlockBumpable[id - 1] = bumpable;
+    blockprop_bumpable[id - 1] = bumpable ? -1 : 0;
+}
+
+short Blocks::GetBlockPlayerFilter(int id)
+{
+    if ((id < 1) || (id > Block::MAX_ID)) return 0;
+    return blockprop_playerfilter[id];
+}
+
+void Blocks::SetBlockPlayerFilter(int id, short characterId)
+{
+    if ((id < 1) || (id > Block::MAX_ID)) return;
+    blockprop_playerfilter[id] = characterId;
+}
+
+// Getter for address of Block property arrays
+uintptr_t Blocks::GetPropertyTableAddress(const std::string& s)
+{
+    if (s == "bumpable")
+    {
+        return reinterpret_cast<uintptr_t>(blockprop_bumpable);
+    }
+    else if (s == "playerfilter")
+    {
+        return reinterpret_cast<uintptr_t>(blockprop_playerfilter);
+    }
+    else
+    {
+        return 0;
+    }
 }

LunaDll/LuaMain/LunaLuaMain.cpp
@@ -655,7 +655,8 @@ void CLunaLua::bindAll()
                 def("__enablePerfTracker", &LuaProxy::Misc::__enablePerfTracker),
                 def("__disablePerfTracker", &LuaProxy::Misc::__disablePerfTracker),
                 def("__getPerfTrackerData", &LuaProxy::Misc::__getPerfTrackerData),
-                def("__getNPCPropertyTableAddress", &NPC::GetPropertyTableAddress)
+                def("__getNPCPropertyTableAddress", &NPC::GetPropertyTableAddress),
+                def("__getBlockPropertyTableAddress", &Blocks::GetPropertyTableAddress)
             ],
 
             namespace_("FileFormats")[

LunaDll/Misc/RuntimeHook.h
@@ -439,5 +439,7 @@ void __stdcall runtimeHookResetSectionMusicWrapperA3576F(void);
 void __stdcall runtimeHookResetSectionMusicWrapperA35E9B(void);
 void __stdcall runtimeHookResetSectionMusicWrapperAA4486(void);
 
+void __stdcall runtimeHookPlayerBouncePushCheckWrapper(void);
+
 #endif
 

LunaDll/Misc/RuntimeHookComponents/RuntimeHookCharacterId.cpp
@@ -61,9 +61,17 @@ public:
         mStoredTemplate.MountColor = 0;
         mStoredTemplate.Hearts = 1;
         memset(mHitbox, 0, 10*sizeof(CharacterHitBoxData));
+        if (mFilterBlock != 0)
+        {
+            Blocks::SetBlockPlayerFilter(mFilterBlock, mId);
+        }
     }
     ~CharacterDataStruct()
     {
+        if (mFilterBlock != 0)
+        {
+            Blocks::SetBlockPlayerFilter(mFilterBlock, 0);
+        }
         for (int powerupId = 1; powerupId <= 10; powerupId++)
         {
             ImageLoader::UnregisterExtraGfx(mName + "-" + std::to_string(powerupId));
@@ -1191,7 +1199,6 @@ static auto patch_animate_hook_0x9E1CA9 = PATCH(0x9E1CA9).CALL(HOOK_0x9E1CA9);
 // Check to allow player to pass through their own filter block type
 __declspec(naked) static void  __stdcall HOOK_0x9A3CC5() {
     __asm {
-        pushf
         push ecx
         push edx
         push esi // Args #2
@@ -1200,12 +1207,10 @@ __declspec(naked) static void  __stdcall HOOK_0x9A3CC5() {
         and dword ptr ds:[ebp-0x54], eax
         pop edx
         pop ecx
-        popf
-        mov eax, dword ptr ds : [0xB25A04]
         ret
     }
 }
-static auto patch_block_passthrough_0x9A3CC5 = PATCH(0x9A3CC5).CALL(HOOK_0x9A3CC5);
+static auto patch_block_passthrough_0x9A3CC5 = PATCH(0x9A3CC5).CALL(HOOK_0x9A3CC5).JMP(0x9A3DD2).NOP_PAD_TO_SIZE<269>();
 
 // Check if this is a hittable switch block... 
 __declspec(naked) static void  __stdcall HOOK_0x9DA747() {
@@ -2252,19 +2257,19 @@ static void __stdcall runtimeHookCharacterIdAnimateBlocks(void)
 
 static int __stdcall runtimeHookCharacterIdBlockPlayerCheck(PlayerMOB* player, int blockIdx)
 {
-    short characterId = (short)player->Identity;
-    if (characterId >= 1 && characterId <= 5)
+    short blockId = Block::GetRaw(blockIdx)->BlockType;
+    short characterFilter = Blocks::GetBlockPlayerFilter(blockId);
+
+    // -1 means allow all characters
+    if (characterFilter == -1)
     {
-        return -1;
+        return 0;
     }
 
-    auto it = runtimeHookCharacterIdMap.find(characterId);
-    if (it != runtimeHookCharacterIdMap.end())
+    short characterId = (short)player->Identity;
+    if (characterFilter == characterId)
     {
-        short filterBlockId = it->second->mFilterBlock;
-        if ((filterBlockId != 0) && (filterBlockId == Block::GetRaw(blockIdx)->BlockType)) {
-            return 0;
-        }
+        return 0;
     }
 
     return -1;

LunaDll/Misc/RuntimeHookComponents/RuntimeHookGeneral.cpp
@@ -913,6 +913,9 @@ void TrySkipPatch()
     // Disable some built-in key press handling code that we don't want to do things
     PATCH(0x8BD952).JMP(0x8BD9B6).Apply();
 
+    // Hook to fix player bounce push not properly ignoring character filter blocks
+    PATCH(0x9C0B3E).JMP(runtimeHookPlayerBouncePushCheckWrapper).NOP_PAD_TO_SIZE<6>().Apply();
+
     /************************************************************************/
     /* Import Table Patch                                                   */
     /************************************************************************/

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -2836,3 +2836,42 @@ _declspec(naked) void __stdcall runtimeHookResetSectionMusicWrapperAA4486(void)
         ret
     }
 }
+
+static int __stdcall runtimeHookPlayerBouncePushCheck(unsigned int blockId, PlayerMOB* player)
+{
+    short characterFilter = Blocks::GetBlockPlayerFilter(blockId);
+
+    // -1 means allow all characters
+    if (characterFilter == -1)
+    {
+        return -1; // No collision
+    }
+
+    short characterId = (short)player->Identity;
+    if (characterFilter == characterId)
+    {
+        return -1; // No collision
+    }
+
+    return 0; // Allow collision
+}
+
+_declspec(naked) void __stdcall runtimeHookPlayerBouncePushCheckWrapper(void)
+{
+    // Overwrites 009C0B3E jne smbx.9C0C19
+    __asm {
+        jne disallowCollide
+        push edx
+        push ebx // player
+        push edi // blockId
+        call runtimeHookPlayerBouncePushCheck
+        pop edx
+        cmp eax, 0
+        jne disallowCollide
+        push 0x9C0B44
+        ret
+    disallowCollide :
+        push 0x9C0C19
+        ret
+    }
+}

LunaDll/SMBXInternal/Blocks.h
@@ -71,6 +71,10 @@ namespace Blocks {
     void InitProperties();
     bool GetBlockBumpable(int id);
     void SetBlockBumpable(int id, bool bumpable);
+    short GetBlockPlayerFilter(int id);
+    void SetBlockPlayerFilter(int id, short characterId);
+
+    uintptr_t GetPropertyTableAddress(const std::string& s);
 }
 
 

commit 34a736c2b29ca420caceb012d97b61dac0982969

Fix accidentaly error with bumpable blocks introduced in 8a2aa1fd7edcfc98565f8b1550a9016265820ecf

LunaDll/SMBXInternal/Blocks.cpp
@@ -152,12 +152,12 @@ void Blocks::InitProperties() {
 
 bool Blocks::GetBlockBumpable(int id) {
     if ((id < 1) || (id > Block::MAX_ID)) return false;
-    return (blockprop_bumpable[id - 1] != 0);
+    return (blockprop_bumpable[id] != 0);
 }
 
 void Blocks::SetBlockBumpable(int id, bool bumpable) {
     if ((id < 1) || (id > Block::MAX_ID)) return;
-    blockprop_bumpable[id - 1] = bumpable ? -1 : 0;
+    blockprop_bumpable[id] = bumpable ? -1 : 0;
 }
 
 short Blocks::GetBlockPlayerFilter(int id)