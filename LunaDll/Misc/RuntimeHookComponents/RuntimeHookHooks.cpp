WohlSoft/LunaLua

commit 33f986cbe28f64f50de2cd53bf2c3bd02ccbd84d

Some tweaks to p-switch handling to avoid block resorting

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -3169,3 +3169,105 @@ _declspec(naked) void __stdcall runtimeHookNPCWalkFixSlope()
         ret
     }
 }
+
+static void markBlocksUnsorted()
+{
+    // NOTE: We re-run this anyway even if GM_BLOCKS_SORTED is already set, to make sure the max
+    //       lookup value is updated based on block count, since that for some reason seems
+    //       necessary.
+    WORD blockCount = GM_BLOCK_COUNT;
+    GM_BLOCKS_SORTED = -1;
+    for (int i = 0; i <= 16000; i++)
+    {
+        GM_BLOCK_LOOKUP_MIN[i] = 1;
+        GM_BLOCK_LOOKUP_MAX[i] = blockCount;
+    }
+}
+
+static void __stdcall runtimeHookAfterPSwitchBlocksReordered(void)
+{
+    markBlocksUnsorted();
+}
+
+_declspec(naked) void __stdcall runtimeHookAfterPSwitchBlocksReorderedWrapper(void)
+{
+    __asm {
+        push eax
+        push ecx
+        push edx
+        call runtimeHookAfterPSwitchBlocksReordered
+        pop edx
+        pop ecx
+        pop eax
+        mov edi, 1
+        push 0x009E450C
+        ret
+    }
+}
+
+static const std::wstring destroyedPSwitchBlockLayerName(L"Destroyed PSwitch Blocks");
+
+static void __stdcall runtimeHookPSwitchStartRemoveBlock(unsigned int blockIdx)
+{
+    Block& b = *Block::GetRaw(blockIdx);
+
+    b.BlockType = 0;
+    b.IsHidden = -1;
+    b.pLayerName = destroyedPSwitchBlockLayerName;
+}
+
+_declspec(naked) void __stdcall runtimeHookPSwitchStartRemoveBlockWrapper(void)
+{
+    __asm {
+        push eax
+        push ecx
+        push edx
+        push edi // Arg 1
+        call runtimeHookPSwitchStartRemoveBlock
+        pop edx
+        pop ecx
+        pop eax
+        push 0x009E3D9E
+        ret
+    }
+}
+
+static unsigned int __stdcall runtimeHookPSwitchGetNewBlockAtEnd(unsigned int blockIdx)
+{
+    unsigned int blockCount = GM_BLOCK_COUNT;
+
+    // TODO: This could be faster if it mattered, and maybe share block reusing with Lua block spawning?
+    for (unsigned int i = 0; i < blockCount; i++)
+    {
+        Block& b = *Block::GetRaw(i);
+        if (b.pLayerName == destroyedPSwitchBlockLayerName)
+        {
+            if (gLunaLua.isValid()) {
+                std::shared_ptr<Event> blockInvalidateEvent = std::make_shared<Event>("onBlockInvalidateForReuseInternal", false);
+                blockInvalidateEvent->setDirectEventName("onBlockInvalidateForReuseInternal");
+                blockInvalidateEvent->setLoopable(false);
+                gLunaLua.callEvent(blockInvalidateEvent, i);
+            }
+
+            return i;
+        }
+    }
+
+    blockCount++;
+    GM_BLOCK_COUNT = blockCount;
+    return blockCount;
+}
+
+_declspec(naked) void __stdcall runtimeHookPSwitchGetNewBlockAtEndWrapper(void)
+{
+    __asm {
+        push ecx
+        push edx
+        call runtimeHookPSwitchGetNewBlockAtEnd
+        pop edx
+        pop ecx
+        mov esi, eax
+        push 0x009E3E71
+        ret
+    }
+}

LunaDll/Defines.h
@@ -335,6 +335,9 @@ DEFMEM(GM_EVENT_COUNT,      WORD,  0x00B2D710);
 // Blocks
 DEFMEM(GM_BLOCK_COUNT,      WORD,  0x00B25956);
 DEFMEM(GM_BLOCKS_PTR,       void*, 0x00B25A04);
+DEFMEM(GM_BLOCKS_SORTED,    WORD,  0x00B2C894);
+DEFMEM(GM_BLOCK_LOOKUP_MIN, WORD*, 0x00B25758);
+DEFMEM(GM_BLOCK_LOOKUP_MAX, WORD*, 0x00B25774);
 
 // Backgrounds
 DEFMEM(GM_SEC_BG_ID,     short*,  0x00B258B8); //Define for sections background id

LunaDll/Misc/RuntimeHook.h
@@ -450,5 +450,9 @@ void __stdcall runtimeHookNPCWalkFixClearTemp();
 void __stdcall runtimeHookNPCWalkFixTempHitConditional();
 void __stdcall runtimeHookNPCWalkFixSlope();
 
+void __stdcall runtimeHookAfterPSwitchBlocksReorderedWrapper(void);
+void __stdcall runtimeHookPSwitchStartRemoveBlockWrapper(void);
+void __stdcall runtimeHookPSwitchGetNewBlockAtEndWrapper(void);
+
 #endif
 

LunaDll/Misc/RuntimeHookComponents/RuntimeHookGeneral.cpp
@@ -942,6 +942,11 @@ void TrySkipPatch()
     PATCH(0xA1BB3A).JMP(runtimeHookNPCWalkFixTempHitConditional).NOP_PAD_TO_SIZE<23>().Apply();
     PATCH(0xA13188).JMP(runtimeHookNPCWalkFixSlope).NOP_PAD_TO_SIZE<167>().Apply();
 
+    // Patch to handle block reorder after p-switch handling
+    PATCH(0x9E441A).JMP(runtimeHookAfterPSwitchBlocksReorderedWrapper).NOP_PAD_TO_SIZE<242>().Apply();
+    PATCH(0x9E3D30).JMP(runtimeHookPSwitchStartRemoveBlockWrapper).NOP_PAD_TO_SIZE<110>().Apply();
+    PATCH(0x9E3E54).JMP(runtimeHookPSwitchGetNewBlockAtEndWrapper).NOP_PAD_TO_SIZE<29>().Apply();
+
     /************************************************************************/
     /* Import Table Patch                                                   */
     /************************************************************************/

commit dde0d4c2d4afcff10d4148c4aeb9f2412ec18b79

Fix mistake in markBlocksUnsorted introduced in 33f986cbe28f64f50de2cd53bf2c3bd02ccbd84d that caused blocks to stop colliding correctly after p-switch trigger

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -3191,11 +3191,11 @@ _declspec(naked) void __stdcall runtimeHookNPCWalkFixSlope()
 
 static void markBlocksUnsorted()
 {
-    // NOTE: We re-run this anyway even if GM_BLOCKS_SORTED is already set, to make sure the max
+    // NOTE: We re-run this anyway even if GM_BLOCKS_SORTED is already cleared, to make sure the max
     //       lookup value is updated based on block count, since that for some reason seems
     //       necessary.
     WORD blockCount = GM_BLOCK_COUNT;
-    GM_BLOCKS_SORTED = -1;
+    GM_BLOCKS_SORTED = 0;
     for (int i = 0; i <= 16000; i++)
     {
         GM_BLOCK_LOOKUP_MIN[i] = 1;