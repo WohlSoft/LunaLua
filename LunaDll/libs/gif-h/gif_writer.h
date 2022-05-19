b9b6be2034f267d836840ed2cbd5df9b5b5747c2
Bluenaxela <Bluenaxela@users.noreply.github..com>
2022-05-19 01:50:42 -0700

    Revert "gif.h: First pass at an experiment for prioritizing palette better."
    This experimental commit was unstable, and will be moved to a branch.
    
    This reverts commit 209410f6d0706bebf9cdc0a5e809f19b523675a9.

--- LunaDll/libs/gif-h/gif_writer.h
+++ LunaDll/libs/gif-h/gif_writer.h
@@ -9,30 +9,6 @@ namespace GIF_H
 
 	struct GifLzwNode;
 
-	struct GifPalette
-	{
-		// k-d tree over RGB space, organized in heap fashion
-		// i.e. left child of node i is node i*2, right child is node i*2+1
-		// nodes 256-511 are implicitly the leaves, containing a color
-		struct Node
-		{
-			uint8_t treeSplitElt;
-			uint8_t treeSplit;
-			int     splitRange;
-		};
-		Node nodes[256];
-
-		struct Entry
-		{
-			uint8_t r;
-			uint8_t g;
-			uint8_t b;
-		};
-		Entry entries[256];
-
-		int bitDepth;
-	};
-
 	struct GifWriter
 	{
 		int height;
@@ -41,7 +17,6 @@ namespace GIF_H
 		uint8_t* oldImage;
 		uint8_t* tempImage;
 		GifLzwNode* tempCodetree;
-		GifPalette pal;
 		bool firstFrame;
 		long int delaypos;
 	};
