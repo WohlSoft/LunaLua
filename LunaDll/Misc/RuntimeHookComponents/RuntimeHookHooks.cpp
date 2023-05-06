WohlSoft/LunaLua

commit fec7282ffcaa41bbc478d915983901fb41dfe1e9

Screenshots and GIF recordings now record at framebuffer size rather than window size.
Behind the scenes, start using seperate framebuffers per-camera, as well as a main framebuffer of the same size to combine them.

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -759,7 +759,12 @@ extern BOOL __stdcall StretchBltHook(
     // If we're copying from our rendering screen, we're done with the frame
     if (hdcSrc == (HDC)GM_SCRN_HDC && g_GLEngine.IsEnabled() && !Renderer::IsAltThreadActive())
     {
-        g_GLEngine.RenderCameraToScreen(hdcDest, nXOriginDest, nYOriginDest, nWidthDest, nHeightDest, hdcSrc, nXOriginSrc, nYOriginSrc, nWidthSrc, nHeightSrc, dwRop);
+
+        int cameraIdx = Renderer::Get().GetCameraIdx();
+        SMBX_CameraInfo* cam = SMBX_CameraInfo::Get(cameraIdx);
+        if (cam == NULL) return FALSE;
+
+        g_GLEngine.RenderCameraToScreen(cameraIdx, cam->renderX, cam->renderY, cam->width, cam->height);
 
         return TRUE;
     }
@@ -1294,7 +1299,14 @@ static void __stdcall PostCameraUpdateHook(int cameraIdx, int maxCameraIdx)
     // Send camera position to GLEngine
     SMBX_CameraInfo::setCameraX(cameraIdx, cameraPos[cameraIdx][0]);
     SMBX_CameraInfo::setCameraY(cameraIdx, cameraPos[cameraIdx][1]);
-    Renderer::Get().StoreCameraPosition(cameraIdx);
+    if (g_GLEngine.IsEnabled())
+    {
+        std::shared_ptr<GLEngineCmd_SetCamera> cmd = std::make_shared<GLEngineCmd_SetCamera>();
+        cmd->mIdx = cameraIdx;
+        cmd->mX = SMBX_CameraInfo::getCameraX(cameraIdx);
+        cmd->mY = SMBX_CameraInfo::getCameraY(cameraIdx);
+        g_GLEngine.QueueCmd(cmd);
+    }
 
     // Start camera render for this camera
     Renderer::Get().StartCameraRender(cameraIdx);

LunaDll/Misc/LoadScreen.cpp
@@ -174,22 +174,18 @@ static void LoadThread(void)
     do {
         updateFinishedFlag(L);
         
+        Renderer::Get().StartFrameRender();
+        Renderer::Get().StartCameraRender(1);
+
         lua_getglobal(L, "onDraw");
         if (lua_pcall(L, 0, 0, 0))
         {
             MessageBoxA(NULL, lua_tostring(L, -1), "LunaLua LoadScreen Error", MB_OK | MB_ICONWARNING);
         }
 
-        Renderer::Get().StartFrameRender();
-
         Renderer::Get().RenderBelowPriority(DBL_MAX);
 
-        // Get window size
-        const auto winState = gWindowSizeHandler.getStateThreadSafe();
-        const int32_t& windowWidth = winState.windowSize.x;
-        const int32_t& windowHeight = winState.windowSize.y;
-
-        g_GLEngine.RenderCameraToScreen(NULL, 0, 0, windowWidth, windowHeight, NULL, 0, 0, g_GLContextManager.GetMainFBWidth(), g_GLContextManager.GetMainFBHeight(), 0);
+        g_GLEngine.RenderCameraToScreen(1, 0, 0, g_GLContextManager.GetMainFBWidth(), g_GLContextManager.GetMainFBHeight());
 
         g_GLEngine.EndFrame(NULL, true);
 

LunaDll/Rendering/FrameCapture.cpp
@@ -26,10 +26,10 @@ public:
             // Bind framebuffer
             fb->Bind();
 
-            g_GLDraw.DrawStretched(0, 0, w, h, &g_GLContextManager.GetBufTex(), 0, 0, w, h, 1.0f, nullptr);
+            g_GLDraw.DrawStretched(0, 0, w, h, &g_GLContextManager.GetCurrentCameraFBTex(), 0, 0, w, h, 1.0f, nullptr);
 
             // Bind old framebuffer
-            g_GLContextManager.BindFramebuffer();
+            g_GLContextManager.BindCameraFB();
         }
     }
 

LunaDll/Rendering/GL/GLContextManager.cpp
@@ -14,7 +14,8 @@ GLContextManager::GLContextManager() :
     hDC(nullptr), hQueueThreadCTX(nullptr), hMainThreadCTX(nullptr),
     mInitialized(false), mHadError(false), mMainThreadCTXApplied(false),
     mMainFBWidth(800), mMainFBHeight(600),
-    mOldPixelFormat(0), mCurrentFB(nullptr), mFramebuffer(nullptr),
+    mOldPixelFormat(0), mCurrentFB(nullptr), mPrimaryFB(nullptr),
+    mCurrentCameraFB(nullptr), mCurrentCameraIdx(0), mCameraFramebuffers(),
     mConstants()
 {
 }
@@ -145,11 +146,18 @@ void GLContextManager::BindScreen() {
     GLERRORCHECK();
 }
 
-void GLContextManager::BindFramebuffer() {
-    if (mFramebuffer == nullptr) return;
+void GLContextManager::BindPrimaryFB() {
+    if (mPrimaryFB == nullptr) return;
 
-    // Bind the main screen framebuffer
-    mFramebuffer->Bind();
+    // Bind the primary framebuffer
+    mPrimaryFB->Bind();
+}
+
+void GLContextManager::BindCameraFB() {
+    if (mCurrentCameraFB == nullptr) return;
+
+    // Bind the current camera framebuffer
+    mCurrentCameraFB->Bind();
 }
 
 void GLContextManager::EnsureMainThreadCTXApplied()
@@ -161,12 +169,27 @@ void GLContextManager::EnsureMainThreadCTXApplied()
     }
 }
 
-void GLContextManager::BindAndClearFramebuffer() {
-    if (mFramebuffer == nullptr) return;
+void GLContextManager::SetActiveCamera(int cameraIdx)
+{
+    if ((cameraIdx < 1) || (cameraIdx > MAX_CAMERAS))
+    {
+        // Invalid index
+        return;
+    }
 
-    // Bind the main screen framebuffer and clear it
-    mFramebuffer->Bind();
-    mFramebuffer->Clear();
+    // Change to 0-based index
+    cameraIdx--;
+
+    bool isCameraBufferBound = (mCurrentCameraFB == mCurrentFB);
+    if (mCurrentCameraIdx != cameraIdx)
+    {
+        mCurrentCameraIdx = cameraIdx;
+        mCurrentCameraFB = mCameraFramebuffers[cameraIdx];
+        if (isCameraBufferBound)
+        {
+            BindCameraFB();
+        }
+    }
 }
 
 // Set up a new context from a HDC
@@ -251,26 +274,28 @@ bool GLContextManager::InitContextFromHDC(HDC hDC) {
 bool GLContextManager::InitFramebuffer() {
     try
     {
-        mFramebuffer = new GLFramebuffer(mMainFBWidth, mMainFBHeight, false);
+        mPrimaryFB = new GLFramebuffer(mMainFBWidth, mMainFBHeight, false);
+        for (int i = 0; i < MAX_CAMERAS; i++)
+        {
+            mCameraFramebuffers[i] = new GLFramebuffer(mMainFBWidth, mMainFBHeight, false);
+        }
+        mCurrentCameraFB = mCameraFramebuffers[mCurrentCameraIdx];
     }
-    catch(...)
+    catch (...)
     {
-        mFramebuffer = nullptr;
+        ReleaseFramebuffer();
         return false;
     }
     return true;
 }
 
 bool GLContextManager::InitProjectionAndState() {
-    if (mFramebuffer == nullptr) return false;
-    
-    const GLDraw::Texture& tex = mFramebuffer->AsTexture();
-
     // Set projection
+    
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     GLERRORCHECK();
-    glOrtho(0.0f, (float)tex.pw, 0.0f, (float)tex.ph, -1.0f, 1.0f);
+    glOrtho(0.0f, (float)mMainFBWidth, 0.0f, (float)mMainFBHeight, -1.0f, 1.0f);
     GLERRORCHECK();
     glColor3f(1, 1, 1);
     GLERRORCHECK();
@@ -291,14 +316,20 @@ bool GLContextManager::InitProjectionAndState() {
     GLERRORCHECK();
     glEnableClientState(GL_TEXTURE_COORD_ARRAY);
     GLERRORCHECK();
-
+    
     return true;
 }
 
 
 void GLContextManager::ReleaseFramebuffer() {
-    delete mFramebuffer;
-    mFramebuffer = nullptr;
+    if (mPrimaryFB) delete mPrimaryFB;
+    mPrimaryFB = nullptr;
+    for (int i = 0; i < MAX_CAMERAS; i++)
+    {
+        if (mCameraFramebuffers[i]) delete mCameraFramebuffers[i];
+        mCameraFramebuffers[i] = nullptr;
+    }
+    mCurrentCameraFB = nullptr;
 }
 
 void GLContextManager::ReleaseContext() {
@@ -333,8 +364,15 @@ void GLContextManager::SetMainFramebufferSize(int width, int height)
     glLoadIdentity();
     glOrtho(0.0f, mMainFBWidth, 0.0f, mMainFBHeight, -100.0f, 100.0f);
 
+    // Delete and re-init framebuffer
     ReleaseFramebuffer();
     InitFramebuffer();
-    BindAndClearFramebuffer();
+
+    // Bind camera framebuffer and clear
+    g_GLContextManager.BindCameraFB();
+    GLFramebuffer* fb = g_GLContextManager.GetCurrentFB();
+    if (fb) fb->Clear();
+
+    // Re-init some state just in case
     InitProjectionAndState();
 }

LunaDll/Rendering/GL/GLContextManager.h
@@ -98,26 +98,48 @@ public:
         int iMAX_ELEMENT_INDEX;
     };
 public:
+    static const int MAX_CAMERAS = 2;
+
     GLContextManager();
     bool Init(HDC hDC);
     bool IsInitialized();
 
     void BindScreen();
-    void BindAndClearFramebuffer();
-    void BindFramebuffer();
-    inline const GLDraw::Texture& GetBufTex()
+    void BindPrimaryFB();
+    void BindCameraFB();
+    inline const GLDraw::Texture& GetPrimaryFBTex()
     {
-        static GLDraw::Texture nullTex(0, 0, 0);
-        if (mFramebuffer == nullptr)
+        static const GLDraw::Texture nullTex(0, 0, 0);
+        if (mPrimaryFB == nullptr)
         {
             return nullTex;
         }
-        return mFramebuffer->AsTexture();
+        return mPrimaryFB->AsTexture();
+    }
+    inline const GLDraw::Texture& GetCurrentCameraFBTex()
+    {
+        static const GLDraw::Texture nullTex(0, 0, 0);
+        if (mCurrentCameraFB == nullptr)
+        {
+            return nullTex;
+        }
+        return mCurrentCameraFB->AsTexture();
+    }
+    inline const GLDraw::Texture& GetCameraFBTex(int cameraIdx)
+    {
+        static const GLDraw::Texture nullTex(0, 0, 0);
+        if ((cameraIdx < 1) || (cameraIdx > MAX_CAMERAS) || (mCameraFramebuffers[cameraIdx-1] == nullptr))
+        {
+            return nullTex;
+        }
+        return mCameraFramebuffers[cameraIdx - 1]->AsTexture();
     }
     inline GLFramebuffer* GetCurrentFB() { return mCurrentFB; }
     inline void SetCurrentFB(GLFramebuffer* fb) { mCurrentFB = fb; }
     void EnsureMainThreadCTXApplied();
     inline const GLConstants& Constants() { return mConstants; }
+
+    void SetActiveCamera(int cameraIdx);
 
 private:
     bool  mInitialized;
@@ -134,7 +156,10 @@ private:
 
     // Framebuffer variables
     GLFramebuffer* mCurrentFB;
-    GLFramebuffer* mFramebuffer;
+    GLFramebuffer* mPrimaryFB;
+    GLFramebuffer* mCurrentCameraFB;
+    int            mCurrentCameraIdx;
+    GLFramebuffer* mCameraFramebuffers[MAX_CAMERAS];
 
     // Constants list
     GLConstants mConstants;

LunaDll/Rendering/GL/GLEngine.cpp
@@ -82,89 +82,9 @@ void GLEngine::InitForHDC(HDC hdcDest)
             mpUpscaleShader = new GLShader(upscaleShaderVertSrc, upscaleShaderFragSrc);
 
             // A little trick to ensure early rendering works
-            RenderCameraToScreen(hdcDest, 0, 0, g_GLContextManager.GetMainFBWidth(), g_GLContextManager.GetMainFBHeight(),
-                hdcDest, 0, 0, g_GLContextManager.GetMainFBWidth(), g_GLContextManager.GetMainFBHeight(), 0);
             EndFrame(hdcDest, false);
         }
     }
-}
-
-BOOL GLEngine::RenderCameraToScreen(HDC hdcDest, int nXOriginDest, int nYOriginDest, int nWidthDest, int nHeightDest,
-    HDC hdcSrc, int nXOriginSrc, int nYOriginSrc, int nWidthSrc, int nHeightSrc,
-    DWORD dwRop)
-{
-    // Load Post-Processing Shader somewhere here
-
-    static HDC cachedHDC = NULL;
-    if (hdcDest == NULL)
-    {
-        if (cachedHDC != NULL)
-        {
-            hdcDest = cachedHDC;
-        }
-        else
-        {
-            return FALSE;
-        }
-    }
-    else
-    {
-        cachedHDC = hdcDest;
-    }
-
-
-    InitForHDC(hdcDest);
-
-    if (!g_GLContextManager.IsInitialized()) return FALSE;
-
-    // Get window size
-    const auto winState = gWindowSizeHandler.getStateThreadSafe();
-    const int32_t& windowWidth = winState.windowSize.x;
-    const int32_t& windowHeight = winState.windowSize.y;
-    const double& xOffset = winState.fbOffset.x;
-    const double& yOffset = winState.fbOffset.y;
-    const double& xScale = winState.fbScale.x;
-    const double& yScale = winState.fbScale.y;
-
-    g_GLDraw.UnbindTexture();
-
-    // Unbind the texture from the framebuffer (Bind screen)
-    g_GLContextManager.BindScreen();
-    
-    // Re-derive expected destination coordinates
-    double assumedXScale = nWidthDest / static_cast<double>(nWidthSrc);
-    double assumedYScale = nHeightDest / static_cast<double>(nHeightSrc);
-    nXOriginDest = static_cast<int>(std::round(nXOriginDest * (xScale / assumedXScale) + xOffset));
-    nYOriginDest = static_cast<int>(std::round(nYOriginDest * (yScale / assumedYScale) + yOffset));
-    nWidthDest = static_cast<int>(std::round(nWidthSrc * xScale));
-    nHeightDest = static_cast<int>(std::round(nHeightSrc * yScale));
-
-    // Set viewport for window size
-    glViewport(0, 0, windowWidth, windowHeight);
-    GLERRORCHECK();
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    GLERRORCHECK();
-    glOrtho(0, static_cast<GLdouble>(windowWidth), static_cast<GLdouble>(windowHeight), 0, -1.0f, 1.0f);
-    GLERRORCHECK();
-
-    // Use upscaling shader if we're upscaling and it compiled right
-    GLShader* upscaleShader = nullptr;
-    if (mpUpscaleShader->isValid() && ((xScale > 1.0f) || (yScale > 1.0f)))
-    {
-        upscaleShader = mpUpscaleShader;
-    }
-
-    // Draw the buffer, flipped/stretched as appropriate
-    g_GLDraw.DrawStretched(nXOriginDest, nYOriginDest, nWidthDest, nHeightDest, &g_GLContextManager.GetBufTex(), nXOriginSrc, nYOriginSrc, nWidthSrc, nHeightSrc, 1.0f, upscaleShader);
-    GLERRORCHECK();
-    glFlush();
-    GLERRORCHECK();
-
-    // Get ready to draw some more
-    g_GLContextManager.BindAndClearFramebuffer();
-
-    return TRUE;
 }
 
 void GLEngine::EndFrame(HDC hdcDest, bool skipFlipToScreen)
@@ -189,58 +109,78 @@ void GLEngine::EndFrame(HDC hdcDest, bool skipFlipToScreen)
 
     if (!skipFlipToScreen)
     {
-        // Bind screen
-        g_GLContextManager.BindScreen();
+        int fbWidth = g_GLContextManager.GetMainFBWidth();
+        int fbHeight = g_GLContextManager.GetMainFBHeight();
+
+        // Primary FB for screenshot and gif recorder purposes
+        g_GLContextManager.BindPrimaryFB();
 
         // Generate screenshot...
         if (mScreenshot) {
-            const auto winState = gWindowSizeHandler.getStateThreadSafe();
-            const int32_t& windowWidth = winState.windowSize.x;
-            const int32_t& windowHeight = winState.windowSize.y;
-            GenerateScreenshot(0, 0, windowWidth, windowHeight);
+            GenerateScreenshot(0, 0, fbWidth, fbHeight);
         }
 
         if (mGifRecorder.isRunning())
         {
-            // Get window size
-            const auto winState = gWindowSizeHandler.getStateThreadSafe();
-            const int32_t& windowWidth = winState.windowSize.x;
-            const int32_t& windowHeight = winState.windowSize.y;
-            GifRecorderNextFrame(0, 0, windowWidth, windowHeight);
+            // Record next frame
+            GifRecorderNextFrame(0, 0, fbWidth, fbHeight);
 
-            GLEngineCmd_DrawSprite cmd;
-            cmd.mXDest = 10;
-            cmd.mYDest = windowHeight - (10 + recImage->getH());
-            cmd.mWidthDest = recImage->getW();
-            cmd.mHeightDest = recImage->getH();
-            cmd.mXSrc = 0;
-            cmd.mYSrc = 0;
-            cmd.mWidthSrc = recImage->getW();
-            cmd.mHeightSrc = recImage->getH();
-            cmd.mImg = recImage;
-            cmd.mOpacity = 1.0;
-            cmd.mMode = GLDraw::RENDER_MODE_ALPHA;
-            cmd.run(*this);
+            const GLSprite* sprite = g_GLTextureStore.SpriteFromLunaImage(recImage);
+            if (sprite)
+            {
+                const GLDraw::Texture tex(sprite->GetTexId(), recImage->getW(), recImage->getH());
+                g_GLDraw.DrawStretched(10, 10, tex.w, tex.h, &tex, 0, tex.h, tex.w, -tex.h, 1.0f, nullptr);
+            }
         }
         else if (mGifRecorder.isEncoding())
         {
-            const auto winState = gWindowSizeHandler.getStateThreadSafe();
-            const int32_t& windowHeight = winState.windowSize.y;
-
-            GLEngineCmd_DrawSprite cmd;
-            cmd.mXDest = 10;
-            cmd.mYDest = windowHeight - (10 + encImage->getH());
-            cmd.mWidthDest = encImage->getW();
-            cmd.mHeightDest = encImage->getH();
-            cmd.mXSrc = 0;
-            cmd.mYSrc = 0;
-            cmd.mWidthSrc = encImage->getW();
-            cmd.mHeightSrc = encImage->getH();
-            cmd.mImg = encImage;
-            cmd.mOpacity = 1.0;
-            cmd.mMode = GLDraw::RENDER_MODE_ALPHA;
-            cmd.run(*this);
+            const GLSprite* sprite = g_GLTextureStore.SpriteFromLunaImage(encImage);
+            if (sprite)
+            {
+                const GLDraw::Texture tex(sprite->GetTexId(), encImage->getW(), encImage->getH());
+                g_GLDraw.DrawStretched(10, 10, tex.w, tex.h, &tex, 0, tex.h, tex.w, -tex.h, 1.0f, nullptr);
+            }
         }
+
+        // Bind screen
+        g_GLContextManager.BindScreen();
+
+        // Get window size
+        const auto winState = gWindowSizeHandler.getStateThreadSafe();
+        const int32_t& windowWidth = winState.windowSize.x;
+        const int32_t& windowHeight = winState.windowSize.y;
+        const double& xOffset = winState.fbOffset.x;
+        const double& yOffset = winState.fbOffset.y;
+        const double& xScale = winState.fbScale.x;
+        const double& yScale = winState.fbScale.y;
+
+        // Derive expected destination coordinates
+        int nXOriginDest = static_cast<int>(std::round(xOffset));
+        int nYOriginDest = static_cast<int>(std::round(yOffset));
+        int nWidthDest = static_cast<int>(std::round(fbWidth * xScale));
+        int nHeightDest = static_cast<int>(std::round(fbHeight * yScale));
+
+        // Set viewport for window size
+        glViewport(0, 0, windowWidth, windowHeight);
+        GLERRORCHECK();
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        GLERRORCHECK();
+        glOrtho(0, static_cast<GLdouble>(windowWidth), static_cast<GLdouble>(windowHeight), 0, -1.0f, 1.0f);
+        GLERRORCHECK();
+
+        // Use upscaling shader if we're upscaling and it compiled right
+        GLShader* upscaleShader = nullptr;
+        if (mpUpscaleShader->isValid() && ((xScale > 1.0f) || (yScale > 1.0f)))
+        {
+            upscaleShader = mpUpscaleShader;
+        }
+
+        // Draw the buffer, flipped/stretched as appropriate
+        g_GLDraw.DrawStretched(nXOriginDest, nYOriginDest, nWidthDest, nHeightDest, &g_GLContextManager.GetPrimaryFBTex(), 0, 0, fbWidth, fbHeight, 1.0f, upscaleShader);
+        GLERRORCHECK();
+        glFlush();
+        GLERRORCHECK();
 
         // Display Frame
         SwapBuffers(hdcDest);
@@ -255,7 +195,9 @@ void GLEngine::EndFrame(HDC hdcDest, bool skipFlipToScreen)
     }
 
     // Bind framebuffer
-    g_GLContextManager.BindAndClearFramebuffer();
+    g_GLContextManager.BindCameraFB();
+    GLFramebuffer* fb = g_GLContextManager.GetCurrentFB();
+    if (fb) fb->Clear();
 }
 
 bool GLEngine::GenerateScreenshot(uint32_t x, uint32_t y, uint32_t w, uint32_t h) {
@@ -292,6 +234,20 @@ bool GLEngine::GenerateScreenshot(uint32_t x, uint32_t y, uint32_t w, uint32_t h
         return false;
     }
 
+    // Flip pixels
+    int wBytes = 3 * static_cast<int>(w);
+    for (int y = 0; y < static_cast<int>(h) / 2; y++)
+    {
+        int y2 = h - 1 - y;
+        BYTE* row1 = &pPixelData[y * w * 3];
+        BYTE* row2 = &pPixelData[y2 * w * 3];
+        for (int i = 0; i < wBytes; i++)
+        {
+            BYTE tmp = row1[i];
+            row1[i] = row2[i];
+            row2[i] = tmp;
+        }
+    }
 
     bool releaseMem = true;
     if (mScreenshotCallback) {
@@ -350,16 +306,6 @@ void GLEngine::GifRecorderNextFrame(uint32_t x, uint32_t y, uint32_t w, uint32_t
     if (glGetError() != GL_NO_ERROR) {
         delete pixData;
         return;
-    }
-
-    // Flip pixels
-    BYTE* tmpRow = &pixData[w * h * 4];
-    for (int y = 0; y < static_cast<int>(h) / 2; y++)
-    {
-        int y2 = h - 1 - y;
-        memcpy(tmpRow, &pixData[y2 * w * 4], w*4);
-        memcpy(&pixData[y2 * w * 4], &pixData[y * w * 4], w * 4);
-        memcpy(&pixData[y * w * 4], tmpRow, w * 4);
     }
 
     mGifRecorder.addNextFrameToProcess(w, h, pixData, timestamp);

LunaDll/Rendering/GL/GLEngine.h
@@ -33,9 +33,6 @@ public:
     inline bool IsBitwiseCompatEnabled() { return mBitwiseCompat; };
 
     void InitForHDC(HDC hdcDest);
-    BOOL RenderCameraToScreen(HDC hdcDest, int nXOriginDest, int nYOriginDest, int nWidthDest, int nHeightDest,
-        HDC hdcSrc, int nXOriginSrc, int nYOriginSrc, int nWidthSrc, int nHeightSrc,
-        DWORD dwRop);
     void EndFrame(HDC hdcDest, bool skipFlipToScreen);
 
     // External commands
@@ -49,7 +46,7 @@ public:
     bool GenerateScreenshot(uint32_t x, uint32_t y, uint32_t w, uint32_t h);
     void GifRecorderNextFrame(uint32_t x, uint32_t y, uint32_t w, uint32_t h);
 
-    inline void SetCamera(double x, double y) { mCameraX = x; mCameraY = y; }
+    inline void SetCameraPositionInScene(double x, double y) { mCameraX = x; mCameraY = y; }
     inline void GetCamera(double &x, double &y) { x = mCameraX; y = mCameraY; }
 
     void SetFramebufferSize();

LunaDll/Rendering/GL/GLEngineCmds.cpp
@@ -15,14 +15,23 @@ void GLEngineCmd_ClearTextures::run(GLEngine& glEngine) const {
     g_GLTextureStore.ClearLunaImageTextures();
 }
 void GLEngineCmd_RenderCameraToScreen::run(GLEngine& glEngine) const {
+    const GLDraw::Texture& tex = g_GLContextManager.GetCameraFBTex(mCamIdx);
+    g_GLContextManager.BindPrimaryFB();
+    g_GLDraw.DrawSprite(
+        { mRenderX, mRenderY, mRenderX + mWidth, mRenderY + mHeight },
+        &tex,
+        { 0.0, 0.0, mWidth, mHeight },
+        1.0f, GLDraw::RENDER_MODE_ALPHA);
+    /*
     glEngine.RenderCameraToScreen(
-        mHdcDest,
-        mXDest, mYDest,
-        mWidthDest, mHeightDest,
-        mHdcSrc,
-        mXSrc, mYSrc,
-        mWidthSrc, mHeightSrc,
-        mRop);
+        nullptr,
+        mRenderX, mRenderY,
+        mWidth, mHeight,
+        nullptr,
+        0, 0,
+        mWidth, mHeight,
+        0);
+    */
 }
 
 void GLEngineCmd_EndFrame::run(GLEngine& glEngine) const {
@@ -284,14 +293,17 @@ void GLEngineCmd_LuaDraw::run(GLEngine& glEngine) const {
         if (mTarget->mFramebuffer != nullptr)
         {
             // Bind old framebuffer
-            g_GLContextManager.BindFramebuffer();
+            g_GLContextManager.BindCameraFB();
         }
     }
 }
 
 void GLEngineCmd_SetCamera::run(GLEngine& glEngine) const
 {
-    glEngine.SetCamera(mX, mY);
+    g_GLContextManager.SetActiveCamera(mIdx);
+    g_GLContextManager.BindCameraFB();
+    g_GLContextManager.GetCurrentFB()->Clear();
+    glEngine.SetCameraPositionInScene(mX, mY);
 }
 
 void GLEngineCmd_CompileShaderObj::run(GLEngine& glEngine) const

commit 4186ad7f6cdbf8a0aee37fb50aea6a32320d9923

Make screenshake work again
Was originally broken by fec7282ffcaa41bbc478d915983901fb41dfe1e9

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -40,6 +40,8 @@
 
 #include "../../libs/PGE_File_Formats/file_formats.h"
 
+#include "../../Misc/VB6RNG.h"
+
 void CheckIPCQuitRequest();
 
 extern HHOOK HookWnd;
@@ -765,7 +767,15 @@ extern BOOL __stdcall StretchBltHook(
         SMBX_CameraInfo* cam = SMBX_CameraInfo::Get(cameraIdx);
         if (cam == NULL) return FALSE;
 
-        g_GLEngine.RenderCameraToScreen(cameraIdx, cam->renderX, cam->renderY, cam->width, cam->height);
+        int xOff = 0;
+        int yOff = 0;
+        if (GM_EARTHQUAKE)
+        {
+            xOff = static_cast<int>(VB6RNG::generateNumber() * GM_EARTHQUAKE * 4) - GM_EARTHQUAKE * 2;
+            yOff = static_cast<int>(VB6RNG::generateNumber() * GM_EARTHQUAKE * 4) - GM_EARTHQUAKE * 2;
+        }
+
+        g_GLEngine.RenderCameraToScreen(cameraIdx, cam->renderX + xOff, cam->renderY + yOff, cam->width, cam->height);
 
         return TRUE;
     }