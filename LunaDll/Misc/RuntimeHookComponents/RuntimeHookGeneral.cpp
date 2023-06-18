WohlSoft/LunaLua

commit e030d6ec48f86b2c1ac855ac22bf4737f68522c4

First pass at "DPI awareness", to avoid Windows trying to blurily rescale things, and setting our default window size based on the scaling configured in Windows.

LunaDll/Misc/RuntimeHookComponents/RuntimeHookGeneral.cpp
@@ -97,22 +97,17 @@ static FramePaddingInfo getWindowFramePadding(HWND hwnd)
         out.bPadSz = 0;
         if (Luna_IsWindowsVistaOrNewer())
         {
-            static HMODULE dwmModule = LoadLibraryA("Dwmapi.dll");
-            if (dwmModule != nullptr)
+            static auto dwmGetWindowsAttribute = Luna_GetProc<HRESULT (__stdcall *)(HWND, DWORD, PVOID, DWORD)>("Dwmapi.dll", "DwmGetWindowAttribute");
+            if (dwmGetWindowsAttribute != nullptr)
             {
-                typedef HRESULT __stdcall dwm_call_t(HWND, DWORD, PVOID, DWORD);
-                static dwm_call_t* dwmGetWindowsAttribute = reinterpret_cast<dwm_call_t*>(GetProcAddress(dwmModule, "DwmGetWindowAttribute"));
-                if (dwmGetWindowsAttribute != nullptr)
+                RECT rc1, rc2;
+                GetWindowRect(hwnd, &rc1);
+                if (dwmGetWindowsAttribute(hwnd, DWMWA_EXTENDED_FRAME_BOUNDS, &rc2, sizeof(rc2)) == S_OK)
                 {
-                    RECT rc1, rc2;
-                    GetWindowRect(hwnd, &rc1);
-                    if (dwmGetWindowsAttribute(hwnd, DWMWA_EXTENDED_FRAME_BOUNDS, &rc2, sizeof(rc2)) == S_OK)
-                    {
-                        out.lPadSz = rc2.left - rc1.left;
-                        out.rPadSz = rc1.right - rc2.right;
-                        out.tPadSz = rc2.top - rc1.top;
-                        out.bPadSz = rc1.bottom - rc2.bottom;
-                    }
+                    out.lPadSz = rc2.left - rc1.left;
+                    out.rPadSz = rc1.right - rc2.right;
+                    out.tPadSz = rc2.top - rc1.top;
+                    out.bPadSz = rc1.bottom - rc2.bottom;
                 }
             }
         }
@@ -584,6 +579,7 @@ LRESULT CALLBACK HandleWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
 {
     static bool inSizeMoveModal = false;
     static bool inSizeModal = false;
+    static bool gotFirstSize = false;
 
     if (hwnd == gMainWindowHwnd)
     {
@@ -601,8 +597,24 @@ LRESULT CALLBACK HandleWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
                 // Calling DefWindowProcW here is a hack to allow unicode window titles, by redirecting to the unicode DefWindowProcW
                 return DefWindowProcW(hwnd, uMsg, wParam, lParam);
             }
+            case WM_SHOWWINDOW:
+            {
+                if (!gotFirstSize)
+                {
+                    gotFirstSize = true;
+                    gWindowSizeHandler.SetInitialWindowSize();
+                    return 0;
+                }
+            }
             case WM_SIZE:
             {
+                if (!gotFirstSize)
+                {
+                    gotFirstSize = true;
+                    gWindowSizeHandler.SetInitialWindowSize();
+                    return 0;
+                }
+
                 // Store size of main window (low word is width, high word is height)
                 static LPARAM latestLParam;
                 latestLParam = lParam;

LunaDll/GlobalFuncs.cpp
@@ -399,6 +399,14 @@ bool IsWindowsVersionOrNewer(DWORD major, DWORD minor)
         conditionMask);
 }
 
+FARPROC Luna_GetProc_impl(const char* libFilename, const char* procName)
+{
+    HMODULE mod = LoadLibraryA(libFilename);
+    if (mod == nullptr) return nullptr;
+
+    return GetProcAddress(mod, procName);
+}
+
 /*!
  * \brief Initializes game root folder variables
  */
@@ -569,29 +577,6 @@ bool vecStrFind(const std::vector<std::wstring>& vecStr, const std::wstring& fin
     }
     return false;
 }
-
-//HMODULE getModule(std::string moduleName)
-//{
-//    HMODULE ret = 0;
-//    if( !(ret = GetModuleHandleA(moduleName.c_str())) ){
-//        ret = LoadLibraryA(moduleName.c_str());
-//    }
-//    return ret;
-//}
-
-//std::wstring getModulePath()
-//{
-//    HMODULE hModule = GetModuleHandleW(NULL);
-//    WCHAR path[MAX_PATH];
-//    int count = GetModuleFileNameW(hModule, path, MAX_PATH);
-//    for(int i = count; i > 3; i--) {
-//        if(path[i] == L'\\') {
-//            path[i] = 0;
-//            break;
-//        }
-//    }
-//    return std::wstring(path);
-//}
 
 // Function to normalize a path, in such a way that all slashes become forward
 // slashes, duplicate consecutive slashes are removed, and trailing slashes are

LunaDll/GlobalFuncs.h
@@ -87,6 +87,20 @@ inline bool Luna_IsWindowsWin8OrNewer()
     return ret;
 }
 
+// Helper function to load functions from DLLs
+// NOTE: Calling this increments the module reference counter each time and isn't so fast
+//       so the result should be cached.
+//       Static variables are potentially a good enough way to cache this result.
+// NOTE: The template argument allows you to avoid reinterpret_cast boilerplate and storing
+//       this in an 'auto' variable.
+FARPROC Luna_GetProc_impl(const char* libFilename, const char* procName);
+template <typename T>
+T Luna_GetProc(const char* libFilename, const char* procName)
+{
+    return reinterpret_cast<T>(Luna_GetProc_impl(libFilename, procName));
+}
+
+
 /// HELPER FUNCTIONS ///
 std::vector<std::wstring> wsplit( std::wstring str, wchar_t delimiter);
 std::vector<std::string> split( std::string str, char delimiter);

LunaDll/Main.cpp
@@ -31,6 +31,7 @@ using std::max;
 #include "Misc/Playground.h"
 #include "Rendering/GL/GLEngine.h"
 #include "Rendering/GL/GLInitTest.h"
+#include "Rendering/WindowSizeHandler.h"
 #include "Misc/AsmPatch.h"
 #include "Misc/LoadScreen.h"
 #include "../LunaLoader/LunaLoaderPatch.h"
@@ -82,21 +83,6 @@ static DWORD __stdcall GetCurrentProcessorNumberXP(void)
 void LunaDLLInit()
 {
     InitGlobals();
-
-    // Be sure we don't get flagged as DPI unaware
-    if (Luna_IsWindowsVistaOrNewer())
-    {
-        HMODULE user32Module = LoadLibraryA("User32.dll");
-        if (user32Module != nullptr)
-        {
-            typedef BOOL __stdcall setProcessDPIAware_t();
-            setProcessDPIAware_t* setProcessDPIAware = reinterpret_cast<setProcessDPIAware_t*>(GetProcAddress(user32Module, "SetProcessDPIAware"));
-            if (setProcessDPIAware != nullptr)
-            {
-                setProcessDPIAware();
-            }
-        }
-    }
 
     //Check for arguments and write them in gStartupSettings
     ParseArgs(splitCmdArgsW(std::wstring(GetCommandLineW())));

LunaDll/Rendering/WindowSizeHandler.cpp
@@ -1,6 +1,7 @@
 #include "WindowSizeHandler.h"
 #include "GL/GLContextManager.h"
 #include "../Globals.h"
+#include "../GlobalFuncs.h"
 #include "../Input/MouseHandler.h"
 
 // Global instance
@@ -52,4 +53,201 @@ void WindowSizeHandler::RecalculateInteral()
 
     // Recalculate framebutton position to match
     gMouseHandler.Recalculate();
+}
+
+// This function tries to set DPI awareness to the maximum level regardless of the OS :-)
+// Returns the level that could be set given the OS
+static int SetDpiAwareness_impl()
+{
+    DECLARE_HANDLE(DPI_AWARENESS_CONTEXT);
+    enum PROCESS_DPI_AWARENESS {
+        PROCESS_DPI_UNAWARE = 0,
+        PROCESS_SYSTEM_DPI_AWARE = 1,
+        PROCESS_PER_MONITOR_DPI_AWARE = 2
+    };
+    enum DPI_AWARENESS {
+        DPI_AWARENESS_INVALID = -1,
+        DPI_AWARENESS_UNAWARE = 0,
+        DPI_AWARENESS_SYSTEM_AWARE = 1,
+        DPI_AWARENESS_PER_MONITOR_AWARE = 2
+    };
+
+    // Requires Vista or newer
+    if (!Luna_IsWindowsVistaOrNewer())
+    {
+        return PROCESS_DPI_UNAWARE;
+    }
+
+    // Approach for Windows 10 version 1703 and newer
+    static auto setProcessDpiAwarenessContext = Luna_GetProc<BOOL(__stdcall *)(DPI_AWARENESS_CONTEXT)>("User32.dll", "SetProcessDpiAwarenessContext");
+    if (setProcessDpiAwarenessContext)
+    {
+        // Try to set per-monitor awareness (v2)
+        if (setProcessDpiAwarenessContext((DPI_AWARENESS_CONTEXT)-4) == TRUE)
+        {
+            return PROCESS_PER_MONITOR_DPI_AWARE;
+        }
+
+        // Try to set per-monitor awareness (v1)
+        if (setProcessDpiAwarenessContext((DPI_AWARENESS_CONTEXT)-3) == TRUE)
+        {
+            return PROCESS_PER_MONITOR_DPI_AWARE;
+        }
+
+        // Try to set system awareness
+        if (setProcessDpiAwarenessContext((DPI_AWARENESS_CONTEXT)-2) == TRUE)
+        {
+            return PROCESS_SYSTEM_DPI_AWARE;
+        }
+
+        // Failing that, check the awareness level that is set
+        static auto getThreadDpiAwarenessContext = Luna_GetProc<DPI_AWARENESS_CONTEXT(__stdcall *)(void)>("User32.dll", "GetThreadDpiAwarenessContext");
+        static auto getAwarenessFromDpiAwarenessContext = Luna_GetProc<DPI_AWARENESS(__stdcall *)(DPI_AWARENESS_CONTEXT)>("User32.dll", "GetAwarenessFromDpiAwarenessContext");
+        if (getThreadDpiAwarenessContext && getAwarenessFromDpiAwarenessContext)
+        {
+            // Note that the values for DPI_AWARENESS line up with PROCESS_DPI_AWARENESS
+            return getAwarenessFromDpiAwarenessContext(getThreadDpiAwarenessContext());
+        }
+    }
+
+    // Approach for Windows 8.1 and newer
+    static auto setProcessDpiAwareness = Luna_GetProc<HRESULT(__stdcall *)(PROCESS_DPI_AWARENESS)>("Shcore.dll", "SetProcessDpiAwareness");
+    if (setProcessDpiAwareness)
+    {
+        // Try to set per-monitor awareness
+        if (setProcessDpiAwareness(PROCESS_PER_MONITOR_DPI_AWARE) == S_OK)
+        {
+            return PROCESS_PER_MONITOR_DPI_AWARE;
+        }
+
+        // Try to set system awareness otherwise
+        if (setProcessDpiAwareness(PROCESS_SYSTEM_DPI_AWARE) == S_OK)
+        {
+            return PROCESS_SYSTEM_DPI_AWARE;
+        }
+
+        // Failing that, check the awareness level that is set
+        static auto getProcessDpiAwareness = Luna_GetProc<HRESULT(__stdcall *)(HANDLE, PROCESS_DPI_AWARENESS*)>("Shcore.dll", "GetProcessDpiAwareness");
+        PROCESS_DPI_AWARENESS ret;
+        if (getProcessDpiAwareness && (getProcessDpiAwareness(NULL, &ret) == S_OK))
+        {
+            return ret;
+        }
+
+        return PROCESS_DPI_UNAWARE;
+    }
+
+    // Approach for Vista and newer
+    static auto setProcessDPIAware = Luna_GetProc<BOOL(__stdcall *)(void)>("User32.dll", "SetProcessDPIAware");
+    if (setProcessDPIAware)
+    {
+        // Try to set DPI Awareness
+        if (setProcessDPIAware() != 0)
+        {
+            return PROCESS_SYSTEM_DPI_AWARE;
+        }
+
+        // Failing that, check the awareness level that is set
+        static auto isProcessDPIAware = Luna_GetProc<BOOL(__stdcall *)(void)>("User32.dll", "IsProcessDPIAware");
+        if (isProcessDPIAware && (isProcessDPIAware() == TRUE))
+        {
+            return PROCESS_SYSTEM_DPI_AWARE;
+        }
+
+        return PROCESS_DPI_UNAWARE;
+    }
+
+    return PROCESS_DPI_UNAWARE;
+}
+
+// Get the "DPI" from Windows
+// NOTE: This isn't a true DPI, but rather represents Windows' configured
+//       scaling factor, with a value of 96 representing "100%"
+static int GetDpi_impl()
+{
+    // Method for Windows 10, version 1607
+    static auto getDpiForWindow = Luna_GetProc<UINT(__stdcall *)(HWND)>("User32.dll", "GetDpiForWindow");
+    if (getDpiForWindow)
+    {
+        return getDpiForWindow(gMainWindowHwnd);
+    }
+
+    // Method for Windows 8.1
+    enum MONITOR_DPI_TYPE {
+        MDT_EFFECTIVE_DPI = 0,
+        MDT_ANGULAR_DPI = 1,
+        MDT_RAW_DPI = 2,
+        MDT_DEFAULT
+    };
+    static auto getDpiForMonitor = Luna_GetProc<HRESULT(__stdcall *)(HMONITOR, MONITOR_DPI_TYPE, UINT*, UINT*)>("Shcore.dll", "GetDpiForMonitor");
+    if (getDpiForMonitor)
+    {
+        UINT dpiX, dpiY;
+        if (getDpiForMonitor(MonitorFromWindow(gMainWindowHwnd, MONITOR_DEFAULTTONEAREST), MDT_EFFECTIVE_DPI, &dpiX, &dpiY) == S_OK)
+        {
+            return (dpiX + dpiY) / 2;
+        }
+    }
+
+    // Method for pre-8.1 Windows
+    HDC hDC = ::GetDC(gMainWindowHwnd);
+    if (hDC)
+    {
+        int dpiX = ::GetDeviceCaps(hDC, LOGPIXELSX);
+        int dpiY = ::GetDeviceCaps(hDC, LOGPIXELSY);
+        ::ReleaseDC(NULL, hDC);
+        return (dpiX + dpiY) / 2;
+    }
+
+    return 96; // Default number
+}
+
+void WindowSizeHandler::InitDPIAwareness()
+{
+    mDpiAwarenessState = SetDpiAwareness_impl();
+}
+
+void WindowSizeHandler::SetInitialWindowSize()
+{
+    // Only do this when we can tell the OS that we have DPI awareness
+    if (mDpiAwarenessState <= 0) return;
+
+    // Get initial client size we'll be modifying
+    // NOTE: GL won't actually be set up now normally... but we can still get the default FB size just fine.
+    RECT rc;
+    rc.left = 0;
+    rc.top = 0;
+    rc.right = g_GLContextManager.GetMainFBWidth();
+    rc.bottom = g_GLContextManager.GetMainFBHeight();
+
+    // Get DPI
+    int dpi = GetDpi_impl();
+
+    // Get scale factor for DPI
+    double dpiScale = static_cast<double>(dpi) / 96.0;
+
+    // Compute scaled size
+    rc.right = static_cast<LONG>(::round(dpiScale * rc.right));
+    rc.bottom = static_cast<LONG>(::round(dpiScale * rc.bottom));
+
+    // Add window frame
+    AdjustWindowRectEx(&rc, GetWindowLong(gMainWindowHwnd, GWL_STYLE),
+        GetMenu(gMainWindowHwnd) != 0, GetWindowLong(gMainWindowHwnd, GWL_EXSTYLE));
+
+    // Get new height/width
+    int newW = rc.right - rc.left;
+    int newH = rc.bottom - rc.top;
+
+    // Center by default (?)
+    MONITORINFO monitorInfo;
+    monitorInfo.cbSize = sizeof(monitorInfo);
+    GetMonitorInfo(MonitorFromWindow(gMainWindowHwnd, MONITOR_DEFAULTTONEAREST), &monitorInfo);
+    RECT& workRect = monitorInfo.rcWork;
+    int newX = (workRect.left + workRect.right - newW) / 2;
+    int newY = (workRect.top + workRect.bottom - newH) / 2;
+    if (newX < workRect.left) newX = workRect.left;
+    if (newY < workRect.top) newY = workRect.top;
+
+    // Adjust the window size
+    SetWindowPos(gMainWindowHwnd, nullptr, newX, newY, newW, newH, SWP_NOZORDER | SWP_NOACTIVATE | SWP_NOCOPYBITS | SWP_NOREDRAW);
 }

LunaDll/Rendering/WindowSizeHandler.h
@@ -21,14 +21,18 @@ public:
 private:
     State mState;
     std::mutex mMutex;
+    int mDpiAwarenessState;
 
     void RecalculateInteral();
+    void InitDPIAwareness();
 
 public:
     WindowSizeHandler(int defaultW, int defaultH) :
         mState{ {defaultW, defaultH}, {0.0, 0.0}, {1.0, 1.0} },
-        mMutex()
+        mMutex(),
+        mDpiAwarenessState()
     {
+        InitDPIAwareness();
     }
 
     // Getters by value (only use from main thread)
@@ -54,6 +58,9 @@ public:
     // Functions to set window (thread safe)
     void SetWindowSize(int w, int h);
     void Recalculate();
+
+    // DPI Awareness functions
+    void SetInitialWindowSize();
 };
 
 // Global instance

commit 2cbdb46bf6d14fcea2c4d819456b8948128e94af

Fix testing mode black window bug.
Initially caused in e030d6ec48f86b2c1ac855ac22bf4737f68522c4 by mistakenly allowing switch/case fallthrough from WM_SHOWWINDOW to WM_SIZE.

LunaDll/Misc/RuntimeHookComponents/RuntimeHookGeneral.cpp
@@ -716,8 +716,9 @@ LRESULT CALLBACK HandleWndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPara
                 {
                     gotFirstSize = true;
                     currentDpi = gWindowSizeHandler.SetInitialWindowSize();
-                    return 0;
                 }
+                // Use default window procedure for WM_SHOWWINDOW
+                return DefWindowProcW(hwnd, uMsg, wParam, lParam);
             }
             case WM_SIZE:
             {