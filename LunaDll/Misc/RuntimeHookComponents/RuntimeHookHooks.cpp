f90bf7e6aa5b08b434dafe97b44f55048cca320a
Spencer Everly <spencer.everly@gmail.com>
2024-01-22 16:56:43 -0600

    Add support for autostart.ini on boot, make error message when nothing is found a bit clearer

--- LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
+++ LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -1619,15 +1619,48 @@ void __stdcall runtimeHookLoadLevel(VB6StrPtr* filename)
     //If the legacy title screen is about to boot, prevent that and go straight to loading the episode
     if(nameST == "intro.lvl" && dirST == gAppPathUTF8)
     {
-        GameAutostart autostarter = GameAutostart::createGameAutostartByStartupEpisodeSettings(gStartupSettings.epSettings);
-        if(autostarter.selectedWldPath == L"")
+        std::string autostartFile = WStr2Str(getLatestConfigFile(L"autostart.ini"));
+        if(!gStartupSettings.epSettings.enabled && file_existsX(autostartFile))
         {
-            std::string msg = "There is no world file loaded upon starting. This means that you booted upon LunaLoader.exe with no arguments regarding selecting a world or level. Please load a world or level upon starting SMBX2 by loading the X2 launcher instead.";
+            // Try reading the autostart.ini file first if there's no settings available
+            GameAutostart autostarter;
+            IniProcessing autostartConfig(autostartFile);
+            if (autostartConfig.beginGroup("autostart"))
+            {
+                bool doAutostart = autostartConfig.value("do-autostart", false).toBool();
+                autostartConfig.endGroup();
+                if (doAutostart)
+                {
+                    // Note: Internally this uses beginGroup and endGroup, so the group won't be open after it
+                    autostarter = GameAutostart::createGameAutostartByIniConfig(autostartConfig);
+
+                    autostartConfig.beginGroup("autostart");
+                    if (autostartConfig.value("transient", false).toBool())
+                    {
+                        remove(autostartFile.c_str());
+                    }
+                    autostartConfig.endGroup();
+                }
+            }
+            autostartConfig.endGroup();
+            LaunchEpisode(autostarter.selectedWldPath, autostarter.saveSlot, autostarter.singleplayer, autostarter.firstCharacter, autostarter.secondCharacter);
+            return;
+        }
+        else if(gStartupSettings.epSettings.enabled && autostartFile.selectedWldPath != L"")
+        {
+            // If there's no autostart file but the command prompt gives out a world path and some other things, we will then boot to the episode from there
+            GameAutostart autostarter = GameAutostart::createGameAutostartByStartupEpisodeSettings(gStartupSettings.epSettings);
+            LaunchEpisode(autostarter.selectedWldPath, autostarter.saveSlot, autostarter.singleplayer, autostarter.firstCharacter, autostarter.secondCharacter);
+            return;
+        }
+        else if(!gStartupSettings.epSettings.enabled && autostartFile.selectedWldPath == L"")
+        {
+            // If there's still nothing, we don't have any settings so we shouldn't continue booting LunaDLL
+            std::string msg = "There is no world file specified on starting LunaLua. This means that you booted LunaLoader.exe with no arguments regarding selecting a world or level. Please load a world or level starting SMBX2 by loading the X2 launcher (Or Command Prompt) instead.";
             MessageBoxA(gMainWindowHwnd, msg.c_str(), "Error", MB_ICONWARNING | MB_OK);
             _exit(0);
+            return;
         }
-        LaunchEpisode(autostarter.selectedWldPath, autostarter.saveSlot, autostarter.singleplayer, autostarter.firstCharacter, autostarter.secondCharacter);
-        return;
     }
 
     if (!GM_CREDITS_MODE)
