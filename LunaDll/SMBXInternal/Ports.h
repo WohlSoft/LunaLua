aeeb3bd3dab5758e0b3d475ab1a47e0986743f5f
Bluenaxela <Bluenaxela@users.noreply.github.com>
2024-02-12 22:47:03 -0700

    Check in translation of PlayerEffects function, unmodified.

---
+++ LunaDll/SMBXInternal/Ports.h
@@ -0,0 +1,12 @@
+#if !defined(SMBXINTERNAL_PORTS_H)
+#define SMBXINTERNAL_PORTS_H
+
+#include "Types.h"
+
+namespace SMBX13 {
+    namespace Ports {
+        void __stdcall PlayerEffects(int16_t& A);
+    }
+}
+
+#endif // !defined(SMBXINTERNAL_PORTS_H)
