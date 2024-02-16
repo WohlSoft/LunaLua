6dd2baf29b5c2c60c18a0095f3ec6aa16bafb477
Bluenaxela <Bluenaxela@users.noreply.github.com>
2024-02-15 22:01:00 -0700

    Fix some operators on VB6Bool so they will behave more consistently in ported code

--- LunaDll/Misc/VB6Bool.h
+++ LunaDll/Misc/VB6Bool.h
@@ -9,12 +9,16 @@ public:
     VB6Bool() :
         raw(0)
     {}
+
+    // Constructors
     VB6Bool(bool val) :
         raw(val ? -1 : 0)
     {}
     VB6Bool(const VB6Bool &other) :
         raw(other.raw)
     {}
+
+    // Assignment operators
     void operator=(const VB6Bool &other)
     {
         raw = other.raw;
@@ -23,11 +27,42 @@ public:
     {
         raw = (other ? -1 : 0);
     }
+
+    // Cast to c++ bool
     operator bool() const {
         return raw != 0;
     }
+
+    // Manual raw access
     int16_t& asInt16() {
         return raw;
+    }
+    const int16_t& asInt16() const {
+        return raw;
+    }
+
+    // Comparison operators
+    bool operator ==(const VB6Bool &other) const
+    {
+        return raw == other.raw;
+    }
+    bool operator ==(const bool other) const
+    {
+        // This is _not_ the most consistent way to compare versus bool, but this ensures "== true"
+        // behaves like it does in the VB6 code if the raw value is neither -1 nor 0
+        int16_t cmpVal = other ? -1 : 0;
+        return raw == cmpVal;
+    }
+    bool operator !=(const VB6Bool &other) const
+    {
+        return raw != other.raw;
+    }
+    bool operator !=(const bool other) const
+    {
+        // This is _not_ the most consistent way to compare versus bool, but this ensures "!= true"
+        // behaves like it does in the VB6 code if the raw value is neither -1 nor 0
+        int16_t cmpVal = other ? -1 : 0;
+        return raw != cmpVal;
     }
 };
 #pragma pack(pop)
