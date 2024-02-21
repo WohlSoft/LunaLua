WohlSoft/LunaLua

commit aad7492cf02dffcb07a33f354bc16ca7fa153925

Add hook to check collision groups when dealing with held NPC collision

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -1950,6 +1950,38 @@ __declspec(naked) void __stdcall runtimeHookNPCHarmlessGrabRaw(void)
     }
 }
 
+static unsigned int __stdcall runtimeHookGrabbedNPCCollisionGroupInternal(int npcAIdx, int npcBIdx)
+{
+    // Check collision group
+    ExtendedNPCFields* extA = NPC::GetRawExtended(npcAIdx);
+    ExtendedNPCFields* extB = NPC::GetRawExtended(npcBIdx);
+
+    if (!gCollisionMatrix.getIndicesCollide(extA->collisionGroup, extB->collisionGroup)) // Check collision matrix
+        return 0; // Collision cancelled
+
+    return -1; // Collision goes ahead
+}
+
+__declspec(naked) void __stdcall runtimeHookGrabbedNPCCollisionGroup(void)
+{
+    __asm {
+        mov eax, dword ptr ss : [ebp - 0x188] // npcBIdx
+        push eax
+        movsx eax, word ptr ss : [ebp - 0x180] // npcAIdx
+        push eax
+        call runtimeHookGrabbedNPCCollisionGroupInternal
+
+        cmp eax, 0 // return value
+        je cancel_collision
+        lea eax, dword ptr ss : [ebp - 180] // Replace what we're patching over
+        push 0xA0C42B
+        ret
+        cancel_collision :
+        push 0xA0C40A
+        ret
+    }
+}
+
 static void __stdcall runtimeHookNPCTerminalVelocity(NPCMOB* npc)
 {
     // Reimplement the NPC terminal velocity behaviour

LunaDll/Misc/RuntimeHook.h
@@ -341,6 +341,8 @@ void __stdcall runtimeHookNPCWaterSplashAnimRaw(short* effectID, Momentum* coor,
 void __stdcall runtimeHookNPCTerminalVelocityRaw(void);
 
 void __stdcall runtimeHookNPCHarmlessGrabRaw(void);
+void __stdcall runtimeHookGrabbedNPCCollisionGroup(void);
+
 void __stdcall runtimeHookCheckInputRaw(void);
 void __stdcall runtimeHookSetHDCRaw(void);
 

LunaDll/Misc/RuntimeHookComponents/RuntimeHookGeneral.cpp
@@ -1720,6 +1720,7 @@ void TrySkipPatch()
     PATCH(0xA0A6FB).CALL(runtimeHookNPCWaterSplashAnimRaw).Apply();
 
     PATCH(0xA0B969).JMP(runtimeHookNPCHarmlessGrabRaw).NOP_PAD_TO_SIZE<183>().Apply();
+    PATCH(0xA0C425).JMP(runtimeHookGrabbedNPCCollisionGroup).NOP_PAD_TO_SIZE<6>().Apply();
 
     PATCH(0xA10136).JMP(runtimeHookNPCTerminalVelocityRaw).NOP_PAD_TO_SIZE<58>().Apply();
 

commit b9ed4055db6217f2c4f37e6e1a34d7cb5d4035b9

Fix held NPC crash in some circumstances, introduced by aad7492

LunaDll/Misc/RuntimeHookComponents/RuntimeHookHooks.cpp
@@ -1979,7 +1979,7 @@ __declspec(naked) void __stdcall runtimeHookGrabbedNPCCollisionGroup(void)
 
         cmp eax, 0 // return value
         je cancel_collision
-        lea eax, dword ptr ss : [ebp - 180] // Replace what we're patching over
+        lea eax, dword ptr ss : [ebp - 0x180] // Replace what we're patching over
         push 0xA0C42B
         ret
         cancel_collision :