53bcc211e6ec4d10f2b813e1478c51825a869af3
Saturnyoshi <actuallysaturnyoshi@gmail.com>
2024-04-27 19:51:19 -0400

    implements walkpastnpcs config

--- LunaDll/SMBXInternal/NPCs.h
+++ LunaDll/SMBXInternal/NPCs.h
@@ -585,6 +585,7 @@ namespace NPC {
     bool GetNotCoinTransformable(int id);
     bool GetStaticDirection(int id);
     bool GetLuaHandlesSpeed(int id);
+    bool GetWalkPastNPCs(int id);
     double GetTerminalVelocity(int id);
 
     uintptr_t GetPropertyTableAddress(const std::string& s);
